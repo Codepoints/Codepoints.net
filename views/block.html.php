669840b912f50645f866e48f7a5ee6a8d68d06c1
Manuel Strehl <git@manuel-strehl.de>
2012-02-26 21:31:54 +0100

    wrap views in div.payload and add nav to blocks

--- views/block.html.php
+++ views/block.html.php
@@ -1,6 +1,5 @@
 <?php
 $title = 'Block ' . $block->getName();
-include "header.php";
 $bounds = $block->getBoundaries();
 $prev = $block->getPrev();
 $next = $block->getNext();
@@ -8,28 +7,39 @@ $plane = $block->getPlane();
 $pagination = new Pagination(count($block->get()));
 $page = isset($_GET['page'])? intval($_GET['page']) : 1;
 $pagination->setPage($page);
+include "header.php";
 ?>
+<div class="payload block">
+  <nav>
+    <ul>
+      <li class="prev">
+        <?php if ($prev):?>
+          <a class="bl" rel="prev" href="<?php e(str_replace(' ', '_', strtolower($prev->getName())))?>"><?php e($prev->getName())?></a>
+        <?php endif?>
+      </li>
+      <li class="up">
+        <a class="pl" rel="up" href="<?php e(str_replace(' ', '_', strtolower($plane->getName())))?>"><?php e($plane->getName())?></a>
+      </li>
+      <li class="next">
+        <?php if ($next):?>
+          <a class="bl" rel="next" href="<?php e(str_replace(' ', '_', strtolower($next->getName())))?>"><?php e($next->getName())?></a>
+        <?php endif?>
+      </li>
+    </ul>
+  </nav>
+  <figure>
+  <img src="static/blocks/<?php e(str_replace(' ', '_', $block->getName()))?>.png" alt=""/>
+  </figure>
   <h1><?php e($title);?></h1>
   <p>From U+<?php f('%04X', $bounds[0])?>
      to U+<?php f('%04X', $bounds[1])?></p>
-  <dl>
-    <?php if ($prev):?>
-      <dt>Previous</dt>
-      <dd><a class="bl" href="<?php e(str_replace(' ', '_', strtolower($prev->getName())))?>"><?php e($prev->getName())?></a></dd>
-    <?php endif?>
-    <?php if ($next):?>
-      <dt>Next</dt>
-      <dd><a class="bl" href="<?php e(str_replace(' ', '_', strtolower($next->getName())))?>"><?php e($next->getName())?></a></dd>
-    <?php endif?>
-    <dt>Plane</dt>
-    <dd><a class="pl" href="<?php e(str_replace(' ', '_', strtolower($plane->getName())))?>"><?php e($plane->getName())?></a></dd>
-  </dl>
   <?php echo $pagination?>
-  <ol class="block">
+  <ol class="block-data">
     <?php foreach ($pagination->getSet($block->get()) as $cp => $na):
       f('<li value="%s"><a class="cp" href="U+%04X" title="%s">%04X<img src="data:%s" alt="" width="16" height="16" /></a></li>',
               $cp, $cp, $na, $cp, $na->getImage());
     endforeach ?>
   </ol>
   <?php echo $pagination?>
+</div>
 <?php include "footer.php"?>
--- views/codepoint.html.php
+++ views/codepoint.html.php
@@ -12,105 +12,107 @@ if ($next):
 endif;
 include "header.php";
 ?>
-    <nav>
-      <ul>
-        <li class="prev">
-          <?php if ($prev):?>
-            <a class="cp" href="U+<?php e($prev)?>"><?php e($prev)?><img src="data:<?php e($prev->getImage())?>" alt="" width="16" height="16" /></a>
-          <?php endif?>
-        </li>
-        <li class="up">
-          <?php $block = $codepoint->getBlock();
-            f('<a class="bl" href="%s">%s</a>', u($block->getName()), $block->getName()); ?>
-        </li>
-        <li class="next">
-          <?php if ($next):?>
-            <a class="cp" href="U+<?php e($next)?>"><?php e($next)?><img src="data:<?php e($next->getImage())?>" alt="" width="16" height="16" /></a>
-          <?php endif?>
-        </li>
-      </ul>
-    </nav>
-    <h1><img src="data:<?php e($codepoint->getImage())?>" alt="" width="16" height="16" /> <?php e($title)?><br/>
-      <?php e($codepoint->getName())?></h1>
-    <section>
-      <h2>Representations</h2>
-      <dl>
-        <dt>Nº</dt>
-        <dd><?php e($codepoint->getId())?></dd>
-        <dt>Your System</dt>
-        <dd><?php if ($props['gc'][0] === 'C'):?>
-            <span class="Cc">&lt;control&gt;</span>
-        <?php else:?>
-            <span>&#<?php e($codepoint->getId())?>;</span>
-        <?php endif?></dd>
-        <dt>UTF-8</dt>
-        <dd><?php e($codepoint->getRepr('UTF-8'))?></dd>
-        <dt>UTF-16</dt>
-        <dd><?php e($codepoint->getRepr('UTF-16'))?></dd>
-        <dt>UTF-32</dt>
-        <dd><?php e($codepoint->getRepr('UTF-32'))?></dd>
-        <?php $alias = $codepoint->getALias();
-        foreach ($alias as $a):?>
-          <dt><?php e($a['type'])?></dt>
-          <dd><?php if ($a['type'] === 'html') {
-                echo '&amp;';
-            }
-            e($a['name']);
-            if ($a['type'] === 'html') {
-                echo ';';
-            }?></dd>
-        <?php endforeach?>
-      </dl>
-    </section>
-    <section>
-      <h2>Properties</h2>
-      <dl>
-        <dt>Unicode version</dt>
-        <dd><?php e($props['age'])?></dd>
-        <dt>Script</dt>
-        <dd><?php e($props['sc'])?></dd>
-        <dt>General Category</dt>
-        <dd><?php e($props['gc'])?></dd>
-        <dt>Bidi Class</dt>
-        <dd><?php e($props['bc'])?></dd>
-        <dt></dt>
-        <dd><?php e($props['age'])?></dd>
-      </dl>
-    </section>
-    <section>
-      <h2>Relations</h2>
-      <dl>
-        <dt>Plane</dt>
-        <dd><?php $plane = $codepoint->getPlane();
-            f('<a class="pl" href="%s">%s</a>', u($plane->name), $plane->name);
-        ?></dd>
-        <dt>Block</dt>
-        <dd><?php $block = $codepoint->getBlock();
-            f('<a class="bl" href="%s">%s</a>', u($block->getName()), $block->getName());
-        ?></dd>
-        <?php if($props['uc'] && $props['uc'] != $codepoint->getId('hex')):?>
-          <dt>Uppercase</dt>
-          <dd></dd>
+<div class="payload codepoint">
+  <nav>
+    <ul>
+      <li class="prev">
+        <?php if ($prev):?>
+          <a class="cp" rel="prev" href="U+<?php e($prev)?>"><?php e($prev)?><img src="data:<?php e($prev->getImage())?>" alt="" width="16" height="16" /></a>
         <?php endif?>
-        <?php if($props['lc'] && $props['lc'] != $codepoint->getId('hex')):?>
-          <dt>Lowercase</dt>
-          <dd></dd>
+      </li>
+      <li class="up">
+        <?php $block = $codepoint->getBlock();
+          f('<a class="bl" rel="up" href="%s">%s</a>', u($block->getName()), $block->getName()); ?>
+      </li>
+      <li class="next">
+        <?php if ($next):?>
+          <a class="cp" rel="next" href="U+<?php e($next)?>"><?php e($next)?><img src="data:<?php e($next->getImage())?>" alt="" width="16" height="16" /></a>
         <?php endif?>
-      </dl>
-    </section>
-    <table>
-      <tbody>
-        <?php foreach ($props as $k => $v):
-              if ($v !== NULL && $v !== '' && $k !== 'cp'):?>
-          <tr class="p_<?php e($k)?>">
-            <th><?php e( array_key_exists($k, $properties)?
-                            str_replace('_', ' ', $properties[$k]) . ' ('.$k.')' :
-                            $k)?></th>
-            <td>
-              <?php e($v)?>
-            </td>
-          </tr>
-        <?php endif; endforeach?>
-      </tbody>
-    </table>
+      </li>
+    </ul>
+  </nav>
+  <h1><img src="data:<?php e($codepoint->getImage())?>" alt="" width="16" height="16" /> <?php e($title)?><br/>
+    <?php e($codepoint->getName())?></h1>
+  <section>
+    <h2>Representations</h2>
+    <dl>
+      <dt>Nº</dt>
+      <dd><?php e($codepoint->getId())?></dd>
+      <dt>Your System</dt>
+      <dd><?php if ($props['gc'][0] === 'C'):?>
+          <span class="Cc">&lt;control&gt;</span>
+      <?php else:?>
+          <span>&#<?php e($codepoint->getId())?>;</span>
+      <?php endif?></dd>
+      <dt>UTF-8</dt>
+      <dd><?php e($codepoint->getRepr('UTF-8'))?></dd>
+      <dt>UTF-16</dt>
+      <dd><?php e($codepoint->getRepr('UTF-16'))?></dd>
+      <dt>UTF-32</dt>
+      <dd><?php e($codepoint->getRepr('UTF-32'))?></dd>
+      <?php $alias = $codepoint->getALias();
+      foreach ($alias as $a):?>
+        <dt><?php e($a['type'])?></dt>
+        <dd><?php if ($a['type'] === 'html') {
+              echo '&amp;';
+          }
+          e($a['name']);
+          if ($a['type'] === 'html') {
+              echo ';';
+          }?></dd>
+      <?php endforeach?>
+    </dl>
+  </section>
+  <section>
+    <h2>Properties</h2>
+    <dl>
+      <dt>Unicode version</dt>
+      <dd><?php e($props['age'])?></dd>
+      <dt>Script</dt>
+      <dd><?php e($props['sc'])?></dd>
+      <dt>General Category</dt>
+      <dd><?php e($props['gc'])?></dd>
+      <dt>Bidi Class</dt>
+      <dd><?php e($props['bc'])?></dd>
+      <dt></dt>
+      <dd><?php e($props['age'])?></dd>
+    </dl>
+  </section>
+  <section>
+    <h2>Relations</h2>
+    <dl>
+      <dt>Plane</dt>
+      <dd><?php $plane = $codepoint->getPlane();
+          f('<a class="pl" href="%s">%s</a>', u($plane->name), $plane->name);
+      ?></dd>
+      <dt>Block</dt>
+      <dd><?php $block = $codepoint->getBlock();
+          f('<a class="bl" href="%s">%s</a>', u($block->getName()), $block->getName());
+      ?></dd>
+      <?php if($props['uc'] && $props['uc'] != $codepoint->getId('hex')):?>
+        <dt>Uppercase</dt>
+        <dd></dd>
+      <?php endif?>
+      <?php if($props['lc'] && $props['lc'] != $codepoint->getId('hex')):?>
+        <dt>Lowercase</dt>
+        <dd></dd>
+      <?php endif?>
+    </dl>
+  </section>
+  <table>
+    <tbody>
+      <?php foreach ($props as $k => $v):
+            if ($v !== NULL && $v !== '' && $k !== 'cp'):?>
+        <tr class="p_<?php e($k)?>">
+          <th><?php e( array_key_exists($k, $properties)?
+                          str_replace('_', ' ', $properties[$k]) . ' ('.$k.')' :
+                          $k)?></th>
+          <td>
+            <?php e($v)?>
+          </td>
+        </tr>
+      <?php endif; endforeach?>
+    </tbody>
+  </table>
+</div>
 <?php include "footer.php"?>
