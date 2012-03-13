556d1c85a85b5f7d3d896ddc25c5b047282d27c5
Manuel Strehl <git@manuel-strehl.de>
2012-03-13 07:38:46 +0100

    add redirection method to router

--- lib/router.class.php
+++ lib/router.class.php
@@ -1,6 +1,9 @@
 <?php
 
 
+/**
+ * handle requests and route them to the appropriate action
+ */
 class Router {
 
     public $baseURL = '/projekte/visual-unicode/';
@@ -11,6 +14,9 @@ class Router {
 
     protected static $defaultRouter;
 
+    /**
+     * singleton: get instance
+     */
     public static function getRouter() {
         if (! self::$defaultRouter) {
             self::$defaultRouter = new self();
@@ -18,11 +24,17 @@ class Router {
         return self::$defaultRouter;
     }
 
+    /**
+     * register an action for an URL
+     */
     public function registerAction($test, $action) {
         $this->actions[] = array($test, $action);
         return $this;
     }
 
+    /**
+     * get the action for an URL
+     */
     public function getAction($url=Null) {
         if ($url === Null) {
             $url = substr($_SERVER['REQUEST_URI'], strlen($this->baseURL));
@@ -36,32 +48,36 @@ class Router {
         return Null;
     }
 
+    /**
+     * register an URL creation scheme for a class
+     */
     public function registerUrl($class, $action) {
         $this->urls[$class] = $action;
         return $this;
     }
 
+    /**
+     * get the URL for an object based on registerUrl input
+     */
     public function getUrl($object) {
         $path = '';
         $class = get_class($object);
-        if (in_array($class, $this->urls)) {
+        if (array_key_exists($class, $this->urls)) {
             $path = $this->urls[$class]($object);
         }
-        switch (get_class($object)) {
-            case "Codepoint":
-                $path = sprintf("U+%04X", $object);
-                break;
-            case "UnicodeBlock":
-                $path = str_replace(' ', '_', strtolower($object->getName()));
-                break;
-            case "UnicodePlane":
-                $path = str_replace(' ', '_', strtolower($object->getName()));
-                if (substr($path, -6) !== '_plane') {
-                    $path .= '_plane';
-                }
-                break;
-        }
         return $this->baseURL . $path;
+    }
+
+    /**
+     * redirect to another URL
+     */
+    public function redirect($url) {
+        if (substr($url, 0, 4) !== 'http' && $url[0] !== '/') {
+            $url = $this->baseUrl . $url;
+        }
+        header('HTTP/1.0 303 See Other');
+        header('Location: ' . $url);
+        exit();
     }
 
 }
