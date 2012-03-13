f7a86c76a2d6b48b0319bcd8707ff01972837e45
Manuel Strehl <git@manuel-strehl.de>
2012-03-13 21:06:40 +0100

    serve empty search

--- views/search.php
+++ views/search.php
@@ -1,13 +1,14 @@
 <?php
-$title = 'Result';
-include "header.php";
 $query = $result->getQuery();
 $cQuery = count($query);
+$title = $cQuery > 0? 'Result' : 'Search';
+include "header.php";
 ?>
 <div class="payload search">
-  <h1><?php e($title);?></h1>
-  <p>Codepoints where
-    <?php foreach ($query as $i => $q):
+  <h1><?php e($title)?></h1>
+  <?php if ($cQuery > 0):?>
+    <p>Codepoints where
+      <?php foreach ($query as $i => $q):
         $sep = ', ';
         if (in_array($cQuery, array(1, $i+1))) {
             $sep = '.';
@@ -17,14 +18,18 @@ $cQuery = count($query);
         printf('<span class="where"><em>%s</em> is <strong>%s</strong></span>%s',
             $info->getCategory($q[0]), ($q[2]? $info->getLabel($q[0], $q[2]) : 'empty'),
             $sep);
-    endforeach ?>
-  </p>
-  <?php echo $pagination?>
-  <ol class="block-data">
-    <?php foreach ($result->get() as $cp => $na):
-      echo '<li value="' . $cp . '">'; cp($na); echo '</li>';
-    endforeach ?>
-  </ol>
-  <?php echo $pagination?>
+      endforeach ?>
+    </p>
+    <?php echo $pagination?>
+    <ol class="block-data">
+      <?php foreach ($result->get() as $cp => $na):
+        echo '<li value="' . $cp . '">'; cp($na); echo '</li>';
+      endforeach ?>
+    </ol>
+    <?php echo $pagination?>
+  <?php else:?>
+    <p>Please enter your search specification.</p>
+  <?php endif?>
+  <?php include "searchform.php"?>
 </div>
 <?php include "footer.php"?>
