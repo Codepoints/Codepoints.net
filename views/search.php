3b186a33e39835c240fec5828a7a38ef26e0c4c4
Manuel Strehl <git@manuel-strehl.de>
2012-03-13 19:54:16 +0100

    update search result

--- views/search.php
+++ views/search.php
@@ -2,21 +2,22 @@
 $title = 'Result';
 include "header.php";
 $query = $result->getQuery();
+$cQuery = count($query);
 ?>
 <div class="payload search">
   <h1><?php e($title);?></h1>
   <p>Codepoints where
     <?php foreach ($query as $i => $q):
-    $sep = ', ';
-    if (in_array(count($query), array(1, $i+1))) {
-        $sep = '';
-    } elseif ($i === count($query) - 2) {
-        $sep = ' and ';
-    }
-    echo '<span class="where"><em>' . $info->getCategory($q[0]) .
-        '</em> is <strong>' . ($q[2]? $info->getLabel($q[0], $q[2]) : 'empty') .
-        '</strong></span>' . $sep;
-    endforeach ?>.
+        $sep = ', ';
+        if (in_array($cQuery, array(1, $i+1))) {
+            $sep = '.';
+        } elseif ($i === $cQuery - 2) {
+            $sep = ' and ';
+        }
+        printf('<span class="where"><em>%s</em> is <strong>%s</strong></span>%s',
+            $info->getCategory($q[0]), ($q[2]? $info->getLabel($q[0], $q[2]) : 'empty'),
+            $sep);
+    endforeach ?>
   </p>
   <?php echo $pagination?>
   <ol class="block-data">
