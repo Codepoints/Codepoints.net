94c25d144298ba967a0c8f0f97538a96aedbcdfe
Manuel Strehl <git@manuel-strehl.de>
2012-03-16 21:16:20 +0100

    fix tooltips and add minor style enhancements

--- static/js/visual-unicode.js
+++ static/js/visual-unicode.js
@@ -130,23 +130,50 @@ function animatePage($this, url, a1, a2) {
   return false;
 }
 
+/**
+ * replace the title attribute with a custom tooltip
+ */
 $.fn.tooltip = function() {
   $('[title]', this).each(function() {
-    var $this = $(this),
-        title = $this.attr('title'),
-        tip = $('<p class="tooltip"></p>').text(title);
-    $this.removeAttr('title');
-    tip.hide().appendTo('body');
-    $this.on('mouseenter', function() {
-      tip.fadeIn('slow').position({
+    var origin = $(this),
+        title = origin.attr('title'),
+        arrow = $('<i></i>'),
+        tip = $('<p class="tooltip"></p>').text(title)
+                .prepend(arrow)
+                .hide().appendTo('body').on('mouseenter', function() {
+                  $(this).hide(); });
+    origin.removeAttr('title')
+          .on('mouseenter', function() {
+      tip.stop(true, true).fadeIn('slow').position({
         my: 'top',
         at: 'bottom',
-        of: $this,
+        of: origin,
         offset: '0 8px',
-        collision: 'fit'
+        collision: 'fit flip',
+        using: function(pos) {
+          var el = $(this);
+          el.css({
+            left: pos.left,
+            top: pos.top
+          });
+          if (pos.left === 0) {
+            arrow.css({ left: '' + (origin.offset().left +
+              (origin.outerWidth()/2)) + 'px' });
+          } else if(pos.left > $(window).width() - tip.outerWidth() - 10) {
+            arrow.css({ left: '' + (origin.offset().left +
+              (origin.outerWidth()/2) - pos.left) + 'px' });
+          } else {
+            arrow.css({ left: '50%' });
+          }
+          if (pos.top < origin.offset().top) {
+            arrow.addClass('down');
+          } else {
+            arrow.removeClass('down');
+          }
+        }
       });
     }).on('mouseleave click', function() {
-      tip.hide();
+      tip.stop(true, true).hide();
     });
   });
   return this;
