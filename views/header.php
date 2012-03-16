1e3e8700686e9942a09b1bafa9f6673bd1beef8b
Manuel Strehl <git@manuel-strehl.de>
2012-03-16 21:15:08 +0100

    incorporate a PageSpeed suggestion

--- views/header.php
+++ views/header.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="UTF-8"/>
     <title><?php e($title)?></title>
+    <link rel="stylesheet" href="static/css/visual-unicode.css"/>
     <script src="static/js/jquery.js"></script>
     <script src="static/js/jquery.ui.js"></script>
     <script src="static/js/json2.js"></script>
     <script src="static/js/underscore.js"></script>
     <script src="static/js/backbone.js"></script>
     <script src="static/js/visual-unicode.js"></script>
-    <link rel="stylesheet" href="static/css/visual-unicode.css"/>
     <?php echo isset($headdata)? $headdata : ''?>
   </head>
   <body>
