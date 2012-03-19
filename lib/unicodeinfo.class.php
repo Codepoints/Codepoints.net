40092e6f5e414134da64d971999885b37099be6e
Manuel Strehl <git@manuel-strehl.de>
2012-03-19 07:14:19 +0100

    add info about boolean props

--- lib/unicodeinfo.class.php
+++ lib/unicodeinfo.class.php
@@ -204,6 +204,15 @@ class UnicodeInfo {
         'scx' => 'Script Extension',
     );
 
+    protected $boolean = array( 'Bidi_M', 'Bidi_C', 'CE', 'Comp_Ex', 'XO_NFC',
+    'XO_NFD', 'XO_NFKC', 'XO_NFKD', 'Join_C', 'Upper', 'Lower', 'OUpper',
+    'OLower', 'CI', 'Cased', 'CWCF', 'CWCM', 'CWL', 'CWKCF', 'CWT', 'CWU',
+    'IDS', 'OIDS', 'XIDS', 'IDC', 'OIDC', 'XIDC', 'Pat_Syn', 'Pat_WS', 'Dash',
+    'Hyphen', 'QMark', 'Term', 'STerm', 'Dia', 'Ext', 'SD', 'Alpha', 'OAlpha',
+    'Math', 'OMath', 'Hex', 'AHex', 'DI', 'ODI', 'LOE', 'WSpace', 'Gr_Base',
+    'Gr_Ext', 'OGr_Ext', 'Gr_Link', 'Ideo', 'UIdeo', 'IDSB', 'IDST',
+    'Radical', 'Dep', 'VS', 'NChar');
+
     protected $legend = array(
 
         'gc' => array(
@@ -535,6 +544,13 @@ class UnicodeInfo {
      */
     public function getCategoryKeys() {
         return array_keys($this->categories);
+    }
+
+    /**
+     * get all category keys, that represent boolean values
+     */
+    public function getBooleanCategories() {
+        return $this->boolean;
     }
 
     /**
