48de40cf260f83b2dcb3729e25c4dcbc155898fc
Manuel Strehl <git@manuel-strehl.de>
2012-03-21 18:40:41 +0100

    add non-existend "About" to nav

--- views/nav.php
+++ views/nav.php
@@ -4,9 +4,10 @@
 <nav>
   <ul>
     <li class="start"><a href="<?php e($router->getUrl())?>" rel="start">Unicode</a></li>
-    <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
     <?php foreach($nav as $rel => $link):?>
       <li class="<?php e($rel)?>"><?php echo $link?></li>
     <?php endforeach?>
+    <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
+    <li class="about"><a href="<?php e($router->getUrl().'about')?>">About</a></li>
   </ul>
 </nav>
