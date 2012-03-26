e9b5f6a4dd6705ef080a81d0bff59ee025ef33f8
Manuel Strehl <git@manuel-strehl.de>
2012-03-26 19:42:28 +0200

    encircle frontpage in .payload

--- views/front.php
+++ views/front.php
@@ -2,10 +2,12 @@
 include "header.php";
 include "nav.php";
 ?>
+<div class="payload front">
   <h1><?php e($title)?></h1>
   <ol>
     <?php foreach ($planes as $plane):?>
       <li><a href="<?php e($router->getUrl($plane))?>"><?php e($plane->name)?></a></li>
     <?php endforeach?>
   </ol>
+</div>
 <?php include "footer.php"?>
