5cb74ec329b73b2e300f2478bb8e2ecc83eaebc7
Manuel Strehl <git@manuel-strehl.de>
2012-03-28 17:59:40 +0200

    add keyboard navigation

--- static/js/visual-unicode.js
+++ static/js/visual-unicode.js
@@ -125,6 +125,43 @@ $(function() {
     }
     return false;
   });
+  $(document).on('keydown', function(e) {
+    if (e.shiftKey && ! e.metaKey && ! e.ctrlKey && ! e.altKey) {
+      var a = [], click = true;
+      switch (e.which) {
+        case 33: // PgDn: paginate back
+          a = $('.pagination .prev a:eq(0)');
+          break;
+        case 34: // PgUp: paginate forth
+          a = $('.pagination .next a:eq(0)');
+          break;
+        case 36: // Pos1: homepage
+          a = $('a[rel="start"]:eq(0)');
+          break;
+        case 37: // ArrL: previous element
+          a = $('a[rel="prev"]:eq(0)');
+          break;
+        case 38: // ArrU: containing block
+          a = $('a[rel="up"]:eq(0)');
+          break;
+        case 39: // ArrR: next element
+          a = $('a[rel="next"]:eq(0)');
+          break;
+        case 40: // ArrD: first child
+          a = $('.data a:eq(0)');
+          click = false;
+          break;
+      }
+      if (a.length) {
+        a.trigger('focus');
+        if (click) {
+          a.click();
+          window.location.href = a[0].href;
+        }
+        return false;
+      }
+    }
+  });
 });
 
 })(this, jQuery);
