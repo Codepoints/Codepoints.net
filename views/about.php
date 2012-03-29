6e68912df53112cc29dfbc49663d359432ed356f
Manuel Strehl <git@manuel-strehl.de>
2012-03-29 23:26:49 +0200

    mention shapecatcher

--- views/about.php
+++ views/about.php
@@ -5,5 +5,17 @@ include 'nav.php';
 ?>
 <div class="payload static">
   <h1><?php e($title)?></h1>
+  <p>The content on this website reflects the information found in<br/>
+  <em>The Unicode Consortium.</em> The Unicode Standard, Version 6.1.0,
+  (Mountain View, CA: The Unicode Consortium, 2012. ISBN 978-1-936213-02-3)<br/>
+  <a href="http://www.unicode.org/versions/Unicode6.1.0/">http://www.unicode.org/versions/Unicode6.1.0/</a>
+  </p>
+  <p>
+    This website is not affiliated with or approved by the Unicode Consortium.
+  </p>
+  <h2>Not Sure about that Character’s Name?</h2>
+  <p>You don’t know the name or properties of a codepoint but its general
+  shape? Fear not, on <a href="http://shapecatcher.com/">Shapecatcher</a>
+  you can draw the character and get it recognized.</p>
 </div>
 <?php include 'footer.php'?>
