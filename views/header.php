073f9c62c943316f0a90dd1c874c98a5b34ffe7a
Manuel Strehl <git@manuel-strehl.de>
2012-04-07 00:16:31 +0200

    add initial-scale to meta

--- views/header.php
+++ views/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <meta name="viewport" content="width=device-width"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
     <title><?php e($title)?></title>
     <link rel="stylesheet" href="static/css/visual-unicode.css"/>
     <link rel="search" href="opensearch.xml" type="application/opensearchdescription+xml" title="Search Codepoints" />
