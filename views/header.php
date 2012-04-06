75bae5cb7e359fcbc2a3028ccaa37848e66aabea
Manuel Strehl <git@manuel-strehl.de>
2012-04-06 22:25:25 +0200

    add meta@viewport element

--- views/header.php
+++ views/header.php
@@ -2,6 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="viewport" content="width=device-width"/>
     <title><?php e($title)?></title>
     <link rel="stylesheet" href="static/css/visual-unicode.css"/>
     <link rel="search" href="opensearch.xml" type="application/opensearchdescription+xml" title="Search Codepoints" />
