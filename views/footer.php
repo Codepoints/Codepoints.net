2e30a289d34ef1c332e7e5c186380056d7fb030d
Manuel Strehl <git@manuel-strehl.de>
2012-04-07 00:15:37 +0200

    add piwik tracker code to footer

--- views/footer.php
+++ views/footer.php
@@ -2,5 +2,8 @@
     <div class="hidden" id="footer_search">
       <?php include "quicksearch.php"?>
     </div>
+<!-- Piwik --> <script type="text/javascript">
+var _paq=_paq||[];(function(){var u="http://piwik.manuel-strehl.de/";_paq.push(['setSiteId',4]);_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.defer=true;g.async=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s);})();
+</script> <!-- End Piwik Code -->
   </body>
 </html>
