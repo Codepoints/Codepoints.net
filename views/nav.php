002c704023204d3fbb74ce4e0311802383874c31
Manuel Strehl <git@manuel-strehl.de>
2012-04-07 00:19:48 +0200

    split nav in twain

--- views/nav.php
+++ views/nav.php
@@ -2,12 +2,14 @@
     $nav = array();
 } ?>
 <nav>
-  <ul>
-    <li class="start"><a href="<?php e($router->getUrl())?>" rel="start">Unicode</a></li>
+  <ul class="primary">
+    <li class="start"><a href="<?php e($router->getUrl())?>" rel="start">Start</a></li>
+    <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
+    <li class="about"><a href="<?php e($router->getUrl().'about')?>">About</a></li>
+  </ul>
+  <ul class="secondary">
     <?php foreach($nav as $rel => $link):?>
       <li class="<?php e($rel)?>"><?php echo $link?></li>
     <?php endforeach?>
-    <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
-    <li class="about"><a href="<?php e($router->getUrl().'about')?>">About</a></li>
   </ul>
 </nav>
