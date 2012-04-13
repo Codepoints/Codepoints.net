535dd6b50289a1aebf8d87ebf7303c1d02ae0f21
Manuel Strehl <git@manuel-strehl.de>
2012-04-13 07:47:57 +0200

    change Sass config

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/projekte/visual-unicode/"
+http_path = "/"
 css_dir = "static/css"
 sass_dir = "static/sass"
 images_dir = "static/images"
