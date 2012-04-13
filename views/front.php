a78492b091681bc1969638fcd2a9c618eeaabe87
Manuel Strehl <git@manuel-strehl.de>
2012-04-13 15:48:14 +0200

    add info to front page

--- views/front.php
+++ views/front.php
@@ -5,12 +5,21 @@ include "nav.php";
 ?>
 <div class="payload front">
   <h1><?php e($title)?></h1>
-  <p>The <a href="<?php e($router->getUrl('planes'))?>">Unicode Planes</a>
-  </p>
-  <ol>
-    <?php foreach ($planes as $plane):?>
-      <li><a href="<?php e($router->getUrl($plane))?>"><?php e($plane->name)?></a></li>
-    <?php endforeach?>
-  </ol>
+  <form method="get" action="<?php e($router->getUrl('SearchResult'))?>" class="searchform">
+    <p><input type="text" name="q" placeholder="Search a Character" />
+       <button type="submit"><span>search</span></button></p>
+  </form>
+  <article>
+    <blockquote class="central">
+      <p><strong>Codepoint</strong>, <em>n.</em> the representation of a character in a computer system.</p>
+    </blockquote>
+    <p>The <a href="<?php e($router->getUrl('planes'))?>">Unicode Planes</a>
+    </p>
+    <ol>
+      <?php foreach ($planes as $plane):?>
+        <li><a href="<?php e($router->getUrl($plane))?>"><?php e($plane->name)?></a></li>
+      <?php endforeach?>
+    </ol>
+  </article>
 </div>
 <?php include "footer.php"?>
