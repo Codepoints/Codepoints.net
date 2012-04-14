99b7480e226b2390293434a42f71f1073f4722ca
Manuel Strehl <git@manuel-strehl.de>
2012-04-14 22:15:17 +0200

    add text to front page

--- views/front.php
+++ views/front.php
@@ -11,11 +11,20 @@ include "nav.php";
   </form>
   <article>
     <blockquote class="central">
-      <p><strong>Codepoint</strong>, <em>n.</em> a position of a character in an encoding system.</p>
+      <p><strong>Codepoint</strong>, <em>n.</em> the position of a character in an encoding system.</p>
     </blockquote>
-    <p>The <a href="<?php e($router->getUrl('planes'))?>">Unicode Planes</a>
-       or maybe you are more daring and want <a href="<?php e($router->getUrl('random'))?>">a random codepoint</a>?
+    <p>All codepoints are arranged in 16 so-called
+       <a href="<?php e($router->getUrl('planes'))?>">planes</a>. These planes
+       are further divided in several blocks with
+       <a href="<?php e($router->getUrl('/basic_latin'))?>">Basic Latin</a>
+       being the first one.
+       You can browse one by one by starting with the first codepoint,
+       <a href="<?php e($router->getUrl('/U+0000'))?>">U+0000</a>.
+       Or maybe you are more daring and want
+       <a href="<?php e($router->getUrl('random'))?>">a random codepoint</a>?
     </p>
+    <h2>All Unicode Planes</h2>
+    <p><i>(as currently defined)</i></p>
     <ol>
       <?php foreach ($planes as $plane):?>
         <li><a href="<?php e($router->getUrl($plane))?>"><?php e($plane->name)?></a></li>
