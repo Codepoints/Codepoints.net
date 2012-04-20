6ddae87311aaa47d57152ec13162cea4f4b47b12
Manuel Strehl <git@manuel-strehl.de>
2012-04-20 15:52:59 +0200

    allow access via full Unicode name

--- index.php
+++ index.php
@@ -259,6 +259,25 @@ $router->addSetting('db', $db)
     $router->redirect(sprintf('U+%04X', $request->data[1]));
 })
 
+->registerAction(function ($url, $o) {
+    // Possible codepoint name, like "LATIN CAPITAL LETTER A"
+    $c = rawurldecode($url);
+    if (preg_match('/^[A-Z][A-Z0-9_ -]{1,127}$/', $c)) {
+        // shortest: "OX", longest has 83 chars (Unicode 6.1)
+        $cp = False;
+        try {
+            $cp = Codepoint::getByName($c, $o['db']);
+        } catch (Exception $e) {
+            return False;
+        }
+        return $cp;
+    }
+    return False;
+}, function($request) {
+    $router = Router::getRouter();
+    $router->redirect(sprintf('U+%04X', $request->data->getId()));
+})
+
 ;
 
 $router->registerUrl('Codepoint', function ($object) {
