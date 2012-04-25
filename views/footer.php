28e75894b4f01b3ebf860a9a3d45989231afec4e
Manuel Strehl <git@manuel-strehl.de>
2012-04-25 17:24:46 +0200

    give tracker code an ID

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
     <div class="hidden" id="footer_search">
       <?php include "quicksearch.php"?>
     </div>
-    <script>
+    <script id="_ts">
       var _paq=_paq||[];(function(){var u="http://piwik.manuel-strehl.de/";_paq.push(['setSiteId',4]);_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.defer=true;g.async=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s);})();
     </script>
     <script src="/static/js/jquery.js"></script>
