dbee0960caa1722df795e008835dd19fb2badfb4
Manuel Strehl <git@manuel-strehl.de>
2012-04-26 07:20:39 +0200

    implement api/login as BrowserID entrypoint

--- index.php
+++ index.php
@@ -88,6 +88,35 @@ $router->addSetting('db', $db)
     $router->redirect(sprintf('U+%04X', $row['cp']));
 })
 
+->registerAction('api/login', function ($request, $o) {
+    // BrowserID login
+    header('Content-Type: application/json');
+
+    if (! isset($_GET['assertation'])) {
+        die('{"status":"error","message":"Missing parameter"}');
+    }
+
+    $ch = curl_init();
+    $data= array('assertation' => $_GET['assertation'],
+                 'audience' => 'http://codepoints.net');
+    //set the url, number of POST vars, POST data
+    curl_setopt($ch, CURLOPT_URL, 'https://browserid.org/verify');
+    curl_setopt($ch, CURLOPT_POST, True);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $state = json_decode($result);
+
+    if ($state === NULL || ! array_key_exists('status', $state)) {
+        die('{"status":"error","message":"Couldn\'t verify assertation"}');
+    } elseif ($state['status'] !== 'okay') {
+        die('{"status":"error","message":"Assertation wrong"}');
+    } else {
+        echo '{"status":"okay"}';
+    }
+})
+
 ->registerAction(array('about', 'glossary'), function ($request, $o) {
     // static pages
     $view = new View($request->trunkUrl);
