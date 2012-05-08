4f85c50cd21f4db2d59fb0786629cfd6db95ad6d
Manuel Strehl <git@manuel-strehl.de>
2012-05-08 07:31:20 +0200

    make blocks special in search fields

--- views/search/fieldset.php
+++ views/search/fieldset.php
@@ -1,22 +1,32 @@
+<?php
+$cat2 = $cat;
+if ($cat === 'blk') {
+    $cat2 = 'block';
+}
+?>
 <fieldset class="propsearch">
   <legend><?php e($info->getCategory($cat))?>:</legend>
-  <?php foreach($info->getLegendForCategory($cat) as $k => $v):
+  <?php
+  foreach($info->getLegendForCategory($cat) as $k => $v):
     $tmp_v = array();
     if (isset($query)) {
         foreach ($query as $q) {
-            if ($q[0] === $cat) {
+            if ($q[0] === $cat2) {
                 $tmp_v = $q[2];
                 break;
             }
         }
     }
+    if ($cat2 === 'block') {
+        $k = $v;
+    }
     ?>
     <p>
-      <input type="checkbox" name="<?php e($cat)?>[]" value="<?php e($k)?>"
-             id="s_<?php e($cat)?>_<?php e($k)?>"
+      <input type="checkbox" name="<?php e($cat2)?>[]" value="<?php e($k)?>"
+             id="s_<?php e($cat2)?>_<?php e($k)?>"
              <?php if (in_array($k, $tmp_v)):?>checked="checked"<?php endif?>
              />
-      <label for="s_<?php e($cat)?>_<?php e($k)?>"><?php if (is_array($v)) {
+      <label for="s_<?php e($cat2)?>_<?php e($k)?>"><?php if (is_array($v)) {
             e(end($v));
         } else {
             e($v);
