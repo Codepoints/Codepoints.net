d1484a9a04b6843d9ead98d8a9161babcf6af384
Manuel Strehl <git@manuel-strehl.de>
2012-05-09 07:48:30 +0200

    add daily CP action

--- index.php
+++ index.php
@@ -372,6 +372,30 @@ $router->addSetting('db', $db)
     }
 })
 
+->registerAction('codepoint_of_the_day', function($request, $o) {
+    // Codepoint of the Day
+    $date = NULL;
+    $today = date('Y-m-d');
+    if (isset($_GET['date'])) {
+        if (preg_match('/^20[0-9]{2}-[01][0-9]-[0-3][0-9]$/', $_GET['date'])) {
+            $date = $_GET['date'];
+        }
+    } else {
+        $date = $today;
+    }
+    if ($date) {
+        list($codepoint, $name, $description) = DailyCP::get($date, $o['db']);
+        if (! $codepoint) {
+            throw new RoutingError();
+        }
+        $view = new View('dailycp');
+        echo $view->render(compact('codepoint', 'description',
+                                   'date', 'today'));
+    } else {
+        throw new RoutingError();
+    }
+})
+
 ->registerAction(function ($url, $o) {
     // Plane
     if (substr($url, -6) === '_plane') {
