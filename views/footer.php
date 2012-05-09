8bce67815e2c06d4dd51b3fc696ef025ecdd8e02
Manuel Strehl <git@manuel-strehl.de>
2012-05-09 18:22:20 +0200

    add footer + footer nav

--- views/footer.php
+++ views/footer.php
@@ -2,6 +2,17 @@
     <div class="hidden" id="footer_search">
       <?php include "quicksearch.php"?>
     </div>
+    <footer class="ft">
+      <nav>
+        <p class="tx">This is Codepoints.net, a site dedicated to all
+        things Unicode and characters.</p>
+        <ul>
+          <li><a href="/">Start</a></li>
+          <li><a href="<?php e($router->getUrl('blog/'))?>">Blog</a></li>
+          <li><a href="<?php e($router->getUrl('about'))?>">About</a></li>
+        </ul>
+      </nav>
+    </footer>
     <script id="_ts">
       var _paq=_paq||[];(function(){var u="http://piwik.manuel-strehl.de/";_paq.push(['setSiteId',4]);_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.defer=true;g.async=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s);})();
     </script>
--- views/nav.php
+++ views/nav.php
@@ -1,26 +1,28 @@
 <?php if (! isset($nav)) {
     $nav = array();
 } ?>
-<nav>
-  <ul class="primary">
-    <li class="start"><a href="<?php e($router->getUrl())?>" rel="start">Start</a></li>
-    <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
-    <?php $stem = array(
-        'http://'.$_SERVER['HTTP_HOST'].$router->getUrl('search?'),
-        'http://'.$_SERVER['HTTP_HOST'].$router->getUrl('wizard?'),
-    );
-    if (array_key_exists('HTTP_REFERER', $_SERVER)):
-        foreach ($stem as $stemX):
-            if (substr($_SERVER['HTTP_REFERER'], 0, strlen($stemX)) === $stemX):?>
-                <li class="up"><a href="<?php e($_SERVER['HTTP_REFERER'])?>">Back to last search</a></li>
-    <?php break; endif; endforeach; endif?>
-    <li class="about"><a href="<?php e($router->getUrl().'about')?>">About</a></li>
-  </ul>
-  <?php if (count($nav)):?>
-    <ul class="secondary">
-      <?php foreach($nav as $rel => $link):?>
-        <li class="<?php e($rel)?>"><?php echo $link?></li>
-      <?php endforeach?>
+<header class="hd">
+  <nav>
+    <ul class="primary">
+      <li class="start"><a href="<?php e($router->getUrl())?>" rel="start">Start</a></li>
+      <li class="search"><a href="<?php e($router->getUrl('SearchResult'))?>" rel="search">Search</a></li>
+      <?php $stem = array(
+          'http://'.$_SERVER['HTTP_HOST'].$router->getUrl('search?'),
+          'http://'.$_SERVER['HTTP_HOST'].$router->getUrl('wizard?'),
+      );
+      if (array_key_exists('HTTP_REFERER', $_SERVER)):
+          foreach ($stem as $stemX):
+              if (substr($_SERVER['HTTP_REFERER'], 0, strlen($stemX)) === $stemX):?>
+                  <li class="up"><a href="<?php e($_SERVER['HTTP_REFERER'])?>">Back to last search</a></li>
+      <?php break; endif; endforeach; endif?>
+      <li class="about"><a href="<?php e($router->getUrl().'about')?>">About</a></li>
     </ul>
-  <?php endif?>
-</nav>
+    <?php if (count($nav)):?>
+      <ul class="secondary">
+        <?php foreach($nav as $rel => $link):?>
+          <li class="<?php e($rel)?>"><?php echo $link?></li>
+        <?php endforeach?>
+      </ul>
+    <?php endif?>
+  </nav>
+</header>
