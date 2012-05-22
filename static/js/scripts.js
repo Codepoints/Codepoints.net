e4035853fdad6b23248cd8ac167a76f3f7c911d6
Manuel Strehl <git@manuel-strehl.de>
2012-05-22 15:36:29 +0200

    enhance script view (more data)

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,218 +1,26 @@
 (function(window, $, undefined) {
 
-var scripts = {
-AD:['Latn'],
-AE:['Arab'],
-AF:['Arab'],
-AL:['Latn'],
-AM:['Armn'],
-AN:['Latn'],
-AO:['Latn'],
-AR:['Latn'],
-AS:['Latn'],
-AT:['Latn'],
-AW:['Latn'],
-AX:['Latn'],
-AZ:['Latn'],
-BA:['Cyrl','Latn'],
-BD:['Beng'],
-BE:['Latn'],
-BF:['Latn'],
-BG:['Cyrl'],
-BH:['Arab'],
-BI:['Latn'],
-BJ:['Latn'],
-BL:['Latn'],
-BN:['Latn'],
-BO:['Latn'],
-BR:['Latn'],
-BT:['Tibt'],
-BY:['Cyrl'],
-CD:['Latn'],
-CF:['Latn'],
-CG:['Latn'],
-CH:['Latn'],
-CI:['Latn'],
-CL:['Latn'],
-CM:['Latn'],
-CN:['Arab','Hans','Latn','Mong','Tibt','Yiii'],
-CO:['Latn'],
-CP:['Latn'],
-CR:['Latn'],
-CU:['Latn'],
-CV:['Latn'],
-CY:['Grek','Latn'],
-CZ:['Latn'],
-DE:['Latn'],
-DJ:['Latn'],
-DK:['Latn'],
-DO:['Latn'],
-DZ:['Arab','Latn'],
-EA:['Latn'],
-EC:['Latn'],
-EE:['Latn'],
-EG:['Arab'],
-EH:['Arab'],
-ER:['Ethi','Latn'],
-ES:['Latn'],
-ET:['Ethi','Latn'],
-FI:['Latn'],
-FJ:['Latn'],
-FM:['Latn'],
-FO:['Latn'],
-FR:['Latn'],
-GA:['Latn'],
-GB:['Latn'],
-GE:['Cyrl','Geor'],
-GF:['Latn'],
-GH:['Latn'],
-GL:['Latn'],
-GN:['Latn'],
-GP:['Latn'],
-GQ:['Latn'],
-GR:['Grek'],
-GT:['Latn'],
-GU:['Latn'],
-GW:['Latn'],
-HK:['Hant'],
-HN:['Latn'],
-HR:['Latn'],
-HT:['Latn'],
-HU:['Latn'],
-IC:['Latn'],
-ID:['Latn'],
-IE:['Latn'],
-IL:['Hebr'],
-IN:['Arab','Beng','Deva','Gujr','Guru','Knda','Latn','Mlym','Orya','Taml','Telu'],
-IQ:['Arab'],
-IR:['Arab'],
-IS:['Latn'],
-IT:['Latn'],
-JO:['Arab'],
-JP:['Jpan'],
-KE:['Latn'],
-KG:['Cyrl'],
-KH:['Khmr'],
-KI:['Latn'],
-KM:['Arab','Latn'],
-KP:['Kore'],
-KR:['Kore'],
-KW:['Arab'],
-KZ:['Cyrl'],
-LA:['Laoo'],
-LB:['Arab'],
-LI:['Latn'],
-LK:['Sinh'],
-LR:['Latn','Vaii'],
-LS:['Latn'],
-LT:['Latn'],
-LU:['Latn'],
-LV:['Latn'],
-LY:['Arab'],
-MA:['Arab','Latn','Tfng'],
-MC:['Latn'],
-MD:['Latn'],
-ME:['Latn'],
-MF:['Latn'],
-MG:['Latn'],
-MH:['Latn'],
-MK:['Cyrl','Latn'],
-ML:['Latn'],
-MM:['Mymr'],
-MN:['Cyrl'],
-MO:['Hant'],
-MQ:['Latn'],
-MR:['Arab'],
-MT:['Latn'],
-MU:['Latn'],
-MV:['Thaa'],
-MW:['Latn'],
-MX:['Latn'],
-MY:['Latn'],
-MZ:['Latn'],
-NA:['Latn'],
-NC:['Latn'],
-NE:['Latn'],
-NG:['Arab','Latn'],
-NI:['Latn'],
-NL:['Latn'],
-NO:['Latn'],
-NP:['Deva'],
-NR:['Latn'],
-NU:['Latn'],
-NZ:['Latn'],
-OM:['Arab'],
-PA:['Latn'],
-PE:['Latn'],
-PF:['Latn'],
-PG:['Latn'],
-PH:['Latn'],
-PK:['Arab'],
-PL:['Latn'],
-PM:['Latn'],
-PR:['Latn'],
-PS:['Arab'],
-PT:['Latn'],
-PW:['Latn'],
-PY:['Latn'],
-QA:['Arab'],
-RE:['Latn'],
-RO:['Latn'],
-RS:['Cyrl','Latn'],
-RU:['Cyrl'],
-RW:['Latn'],
-SA:['Arab'],
-SC:['Latn'],
-SD:['Arab','Latn'],
-SE:['Latn'],
-SI:['Latn'],
-SJ:['Latn'],
-SK:['Latn'],
-SM:['Latn'],
-SN:['Latn'],
-SO:['Latn'],
-SR:['Latn'],
-SS:['Latn'],
-ST:['Latn'],
-SV:['Latn'],
-SY:['Arab','Latn'],
-TD:['Latn'],
-TG:['Latn'],
-TH:['Thai'],
-TJ:['Cyrl'],
-TK:['Latn'],
-TL:['Latn'],
-TM:['Latn'],
-TN:['Arab','Latn'],
-TO:['Latn'],
-TR:['Latn'],
-TV:['Latn'],
-TW:['Hant','Latn'],
-TZ:['Latn'],
-UA:['Cyrl'],
-UG:['Latn'],
-US:['Cher','Latn'],
-UY:['Latn'],
-UZ:['Cyrl'],
-VA:['Latn'],
-VE:['Latn'],
-VN:['Latn'],
-VU:['Latn'],
-WF:['Latn'],
-WS:['Latn'],
-YE:['Arab'],
-YT:['Latn'],
-ZA:['Latn'],
-ZM:['Latn'],
-ZW:['Latn']
-};
-
 function showDetails(obj) {
-  var co = obj.id.substr(0, 2);
-  if (co in scripts) {
-    $.getJSON('/script/'+scripts[co].join(' '), function(data) {
+  var co = obj.id;
+  if (obj.properties.scripts) {
+    $.getJSON('/script/' + obj.properties.scripts.join(' ') +
+              ' ' + obj.properties.oldscripts.join(' ')).done(function(data) {
       var d = $('<div></div>'), sc;
-      $.each(scripts[co], function(i, sc) {
+      $.each(obj.properties.scripts, function(i, sc) {
+        if (sc in data && data[sc]) {
+          d.append(
+            $('<section></section>').append(
+              $('<h3></h3>').append(
+                $('<a></a>').text(data[sc].name)
+                            .attr('href', '/search?sc='+sc)))
+            .append($('<div></div>').html(data[sc].abstract)
+                    .append('<p><small>Source: <a href="'+data[sc].src+'">Wikipedia</a></small></p>')));
+        }
+      });
+      if (obj.properties.oldscripts.length) {
+        d.append('<h2>Old Scripts</h2>');
+      }
+      $.each(obj.properties.oldscripts, function(i, sc) {
         if (sc in data && data[sc]) {
           d.append(
             $('<section></section>').append(
@@ -224,13 +32,20 @@ function showDetails(obj) {
         }
       });
       d.dialog({
-        title: obj.properties.name,
+        title: 'Scripts used in ' + obj.properties.name,
         width: $(window).width() - 40,
         modal: true,
         resizable: false
-      }).on('click', function() {
-        $(this).dialog('close');
       });
+    }).fail(function() {
+      showDetails({id: '___not_found___'});
+    });
+  } else {
+    $('<div>We don’t know about '+obj.properties.name+'’s scripts, sorry!</div>').dialog({
+      title: 'Nothing Found',
+      width: $(window).width() - 40,
+      modal: true,
+      resizable: false
     });
   }
 }
@@ -254,7 +69,7 @@ var svg = d3.select("#earth")
     .attr("height", 800)
     .on("mousedown", mousedown);
 
-d3.json("/static/world-countries.json", function(collection) {
+d3.json("/static/world.json", function(collection) {
   feature = svg.selectAll("path")
       .data(collection.features)
     .enter().append("svg:path")
@@ -262,7 +77,7 @@ d3.json("/static/world-countries.json", function(collection) {
 
   feature.append("svg:title")
       .text(function(d) { return d.properties.name; });
-  feature.on('click', function(e) { console.log(e); showDetails(e); });
+  feature.on('click', function(e) { showDetails(e); });
 
 //  svg.append('svg:circle')
 //      .attr('cx', 640)
