98eab93f1890817a9cf14b05624ffdc977cfaac4
Manuel Strehl <git@manuel-strehl.de>
2012-05-23 19:39:54 +0200

    add list of scripts (forgot to commit controller

--- index.php
+++ index.php
@@ -112,10 +112,24 @@ $router->addSetting('db', $db)
     }
 })
 
-->registerAction(array('about', 'glossary', 'scripts'), function ($request, $o) {
+->registerAction(array('about', 'glossary'), function ($request, $o) {
     // static pages
     $view = new View($request->trunkUrl);
     echo $view->render();
+})
+
+->registerAction('scripts', function ($request, $o) {
+    // scripts
+    $cur = $o['db']->prepare('SELECT iso, name,
+        -- (SELECT abstract FROM script_abstract
+        --   WHERE script_abstract.sc = scripts.iso) abstract,
+        (SELECT COUNT(*) FROM codepoint_script
+          WHERE codepoint_script.sc = scripts.iso) count
+        FROM scripts');
+    $cur->execute();
+    $scripts = $cur->fetchAll(PDO::FETCH_ASSOC);
+    $view = new View($request->trunkUrl);
+    echo $view->render(array('scripts' => $scripts));
 })
 
 ->registerAction('wizard', function ($request, $o) {
