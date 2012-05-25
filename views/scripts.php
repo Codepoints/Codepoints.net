82e44fb08a8a04a3874de11eebaeae629e262d19
Manuel Strehl <git@manuel-strehl.de>
2012-05-25 07:54:44 +0200

    restrict #space to 600px

--- views/scripts.php
+++ views/scripts.php
@@ -8,6 +8,11 @@ svg {
   pointer-events: all;
 }
 
+#space {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
 circle {
   fill: #dbe4f0;
   fill: #b5cbe8;
@@ -55,23 +60,25 @@ include 'nav.php';
 ?>
 <div class="payload static script">
   <h1>Browse Codepoints by Script</h1>
-  <section id="space" class="bk">
+  <section class="bk">
     <p>Scripts used around the world. Drag the globe to rotate it.
        Click on a country to see scripts used there.</p>
-    <svg xmlns="http://www.w3.org/2000/svg" id="earth"
-         width="100%" viewBox="0 0 800 800">
-      <defs>
-        <radialGradient id="reflect"
-          r="75%" cx="35%" cy="20%">
-          <stop stop-color="white" stop-opacity=".67" offset="0" />
-          <stop stop-color="white" stop-opacity="0.0" offset=".3" />
-          <stop stop-color="white" stop-opacity="0.0" offset=".8" />
-          <stop stop-color="black" stop-opacity="0.2" offset="1" />
-        </radialGradient>
-      </defs>
-      <circle cx="50%" cy="50%" r="50%" style="cursor: move" />
-      <circle id="athmo" cx="50%" cy="50%" r="50%" style="pointer-events: none; fill: url(#reflect)" />
-    </svg>
+    <div id="space">
+      <svg xmlns="http://www.w3.org/2000/svg" id="earth"
+           width="100%" viewBox="0 0 800 800">
+        <defs>
+          <radialGradient id="reflect"
+            r="75%" cx="35%" cy="20%">
+            <stop stop-color="white" stop-opacity=".67" offset="0" />
+            <stop stop-color="white" stop-opacity="0.0" offset=".3" />
+            <stop stop-color="white" stop-opacity="0.0" offset=".8" />
+            <stop stop-color="black" stop-opacity="0.2" offset="1" />
+          </radialGradient>
+        </defs>
+        <circle cx="50%" cy="50%" r="50%" style="cursor: move" />
+        <circle id="athmo" cx="50%" cy="50%" r="50%" style="pointer-events: none; fill: url(#reflect)" />
+      </svg>
+    </div>
   </section>
   <section class="bk">
     <dl id="sclist">
