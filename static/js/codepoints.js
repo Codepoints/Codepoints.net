cc0ec41d6b9888ccb842c3ab9d9653f52e1ca511
Manuel Strehl <git@manuel-strehl.de>
2012-05-29 07:49:02 +0200

    add slide effects to about page

--- static/js/codepoints.js
+++ static/js/codepoints.js
@@ -439,6 +439,37 @@ $(function() {
 
   });
 
+  var about = $('.payload.about');
+
+  if (about.length) {
+    var sect = about.find('>section'), n = 0, sel = [];
+    if (sect.filter(window.location.hash).length) {
+      n = sect.filter(window.location.hash).index();
+      window.scrollTo(0,0);
+    }
+    sect.filter(':not(:eq('+n+'))').hide();
+    sect.each(function() {
+      var $this = $(this), $next = $this.next('section');
+      sel.push('#' + this.id);
+      if ($next.length) {
+        $this.append('<p><a href="#'+$next[0].id+'"><i>Read on:</i> “'+$next.find('h1').text()+'”</a></p>');
+      }
+    });
+    $(document).on('click tap', 'a[href^="#"]', function() {
+      var t, h = this.hash;
+      if ($.inArray(h, sel) > -1) {
+        t = $(h);
+        if (t.filter(':hidden').length) {
+          sect.not(t).slideUp();
+          window.location.hash = h;
+          scrollElement.animate({scrollTop:0}, 300);
+          t.slideDown();
+        }
+        return false;
+      }
+    });
+  }
+
 });
 
 })(this, jQuery);
