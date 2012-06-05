9f2f9bdb54b4ade1d1e93a6d63036fd4e820758b
Manuel Strehl <git@manuel-strehl.de>
2012-06-05 15:19:28 +0200

    reference _.js if DEBUG is false

--- views/footer.php
+++ views/footer.php
@@ -19,9 +19,13 @@
     <script id="_ts">
       var _paq=_paq||[];(function(){var u="http://piwik.manuel-strehl.de/";_paq.push(['setSiteId',4]);_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.defer=true;g.async=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s);})();
     </script>
+<?php if(CP_DEBUG):?>
     <script src="/static/js/jquery.js"></script>
     <script src="/static/js/jquery.ui.js"></script>
     <script src="/static/js/codepoints.js"></script>
+<?php else:?>
+    <script src="/static/js/_.js"></script>
+<?php endif?>
     <?php if (isset($footer_scripts)): foreach($footer_scripts as $sc):?>
         <script src="<?php e($sc)?>"></script>
     <?php endforeach; endif?>
