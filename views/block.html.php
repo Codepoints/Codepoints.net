dfdb275a7d7858b28bdd3492d500ac885af51995
Manuel Strehl <git@manuel-strehl.de>
2012-07-10 22:41:57 +0200

    make block view lazier to fix #3

--- views/block.html.php
+++ views/block.html.php
@@ -1,12 +1,14 @@
 <?php
 $title = 'Block ' . $block->getName();
-$block_limits = $block->getBoundaries();
+$block_limits = $block->getBlockLimits();
 $prev = $block->getPrev();
 $next = $block->getNext();
 $plane = $block->getPlane();
-$pagination = new Pagination(count($block->get()));
+$pagination = new Pagination($block->count());
 $page = isset($_GET['page'])? intval($_GET['page']) : 1;
 $pagination->setPage($page);
+$block->slice(($page - 1) * 256, 256);
+$cps = $block->get();
 $hDescription = sprintf('The Unicode block %s contains the codepoints from U+%04X to U+%04X.',
     $block->getName(), $block_limits[0], $block_limits[1]);
 $canonical = $router->getUrl($block);
@@ -26,29 +28,29 @@ include "nav.php";
     <img src="/static/images/blocks/<?php e(str_replace(' ', '_', $block->getName()))?>.png" alt="" width="128" height="128" />
   </figure>
   <h1><?php e($block->getName());?></h1>
-  <?php $defLimits = $block->getBlockLimits(); if ($block_limits === Null):?>
-    <p>This block has not defined any codepoints between U+<?php f('%04X', $defLimits[0])?>
-       and U+<?php f('%04X', $defLimits[1])?>.</p>
+  <?php if (count($cps) === 0):?>
+    <p>This block has not defined any codepoints between U+<?php f('%04X', $block_limits[0])?>
+       and U+<?php f('%04X', $block_limits[1])?>.</p>
   <?php else:?>
     <p>Block from U+<?php f('%04X', $block_limits[0])?>
        to U+<?php f('%04X', $block_limits[1])?></p>
-    <p><a href="http://www.unicode.org/charts/PDF/U<?php f('%04X', $block_limits[0])?>.pdf">Chart at Unicode.org</a> (PDF)</p>
+    <p><a href="http://www.unicode.org/charts/PDF/U<?php f('%04X', $block_limits[0])?>.pdf">Chart at Unicode.org</a> (PDF)<br/>
+       <a href="http://decodeunicode.org/<?php e(str_replace(' ', '_', strtolower($block->getName())))?>">Block at Decode Unicode</a></p>
     <?php echo $pagination?>
-      <ol class="block data">
-        <?php
-        $limits = $pagination->getLimits();
-        $cps = $block->get();
-        for ($i = $limits[0]; $i < $limits[1]; $i++) {
-            if ($i + $block_limits[0] > $block_limits[1]) {
-                break;
-            }
-            if (array_key_exists($i + $block_limits[0], $cps)) {
-                echo '<li value="' . ($i + $block_limits[0]) . '">'; cp($cps[$i + $block_limits[0]]); echo '</li>';
-            } else {
-                echo '<li class="missing" value="'.($i + $block_limits[0]).'"><span>'.sprintf('%04X', $i + $block_limits[0]).'</span></li>';
-            }
-        } ?>
-      </ol>
+    <ol class="block data">
+      <?php
+      $limits = $pagination->getLimits();
+      for ($i = $limits[0]; $i < $limits[1]; $i++) {
+          if ($i + $block_limits[0] > $block_limits[1]) {
+              break;
+          }
+          if (array_key_exists($i + $block_limits[0], $cps)) {
+              echo '<li value="' . ($i + $block_limits[0]) . '">'; cp($cps[$i + $block_limits[0]]); echo '</li>';
+          } else {
+              echo '<li class="missing" value="'.($i + $block_limits[0]).'"><span>'.sprintf('%04X', $i + $block_limits[0]).'</span></li>';
+          }
+      } ?>
+    </ol>
     <?php echo $pagination?>
   <?php endif?>
 </div>
