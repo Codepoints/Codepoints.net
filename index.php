283366faae6a934522104f2b4ebd3c205d115b11
Manuel Strehl <git@manuel-strehl.de>
2012-07-18 20:47:45 +0200

    rm unnecessary log statements

--- index.php
+++ index.php
@@ -65,11 +65,6 @@ function flog($msg) {
 }
 
 
-if (CP_DEBUG) {
-    flog(sprintf("=============\n>>> START %s", $_SERVER['REQUEST_URI']));
-}
-
-
 if (! count($_GET) && ! count($_POST)) {
     $cache = new Cache();
     $cData = $cache->fetch(ltrim($_SERVER['REQUEST_URI'], "/"));
@@ -153,11 +148,6 @@ if ($router->callAction() === False) {
     $cps = codepoint::getForString(rawurldecode($req->trunkUrl), $db);
     $view = new View('error404');
     echo $view->render(compact('planes', 'block', 'plane', 'cps'));
-}
-
-
-if (CP_DEBUG) {
-    flog(sprintf("\n>>> END %s\n===================", $_SERVER['REQUEST_URI']));
 }
 
 
