51707cb00ed94e502c5353c1c114e6d1ac5539aa
Manuel Strehl <m.strehl@kinetiqa.de>
2013-01-16 15:00:55 +0100

    refine embedding styles for minimal sizes

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ define('CP_DEBUG', 0);
 /**
  * cache busting string (the Makefile will manipulate this line)
  */
-define('CACHE_BUST', 'cd35334c69d803deef177978dce58e4864976024');
+define('CACHE_BUST', '96f5859939840699bfe8d09b48c360410a8e4817');
 
 
 /* enable gzip compression of HTML */
