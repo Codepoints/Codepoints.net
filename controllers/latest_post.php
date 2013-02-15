602b2b88b1cc8a1c7cf944727abe1986495ae177
Manuel Strehl <git@manuel-strehl.de>
2013-02-15 19:25:30 +0100

    fix syntax error in latest_post controller

--- controllers/latest_post.php
+++ controllers/latest_post.php
@@ -4,7 +4,8 @@
 $router->registerAction('blog-preview', function ($request, $o) {
     $cachefile = dirname(dirname(__FILE__)).'/cache/blog-preview';
     if (! file_exists($cachefile) || filemtime($cachefile) < time() - 60*60) {
-        $data = json_decode('{'.explode('{', substr(file_get_contents('http://blog.codepoints.net/api/read/json?start=0&num=1&filter=text'), 0, -2), 2)[1], true);
+        $data = explode('{', substr(file_get_contents('http://blog.codepoints.net/api/read/json?start=0&num=1&filter=text'), 0, -2), 2);
+        $data = json_decode('{'.$data[1], true);
         if ($data) {
             $content = sprintf('<h2><a href="%1$s">%2$s</a></h2><p>%3$s <a href="%1$s">...read on</a>.</p>',
                 $data['posts'][0]['url-with-slug'],
