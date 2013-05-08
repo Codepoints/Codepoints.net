d3a24902003354fba76c1759776a26182e6cea04
Manuel Strehl <git@manuel-strehl.de>
2013-05-08 19:09:56 +0200

    fix $.cachedAjax to be really caching

--- src/js/components/jquery.cachedajax.js
+++ src/js/components/jquery.cachedajax.js
@@ -10,14 +10,16 @@ define(['jquery'], function($) {
     if (url in Cache) {
       // a cached response exists
       r = $.when(Cache[url]);
-      if ('success' in options) {
+      if (options && 'success' in options) {
         // call the success callback, if one is given
         options.success(Cache[url]);
       } else if ($.isFunction(options)) {
         options(Cache[url]);
       }
     } else {
-      r = $.get(url, options);
+      r = $.get(url, options).done(function(data) {
+        Cache[url] = data;
+      });
     }
     return r;
   };
