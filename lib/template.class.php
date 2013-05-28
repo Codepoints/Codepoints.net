49e9f1b03f19dc370b4d95b80eb420cdafd334b1
Manuel Strehl <git@manuel-strehl.de>
2013-05-29 00:07:31 +0200

    add caching to Mustache

--- lib/template.class.php
+++ lib/template.class.php
@@ -7,24 +7,23 @@
 class Template {
 
     protected $mustache = null;
-    protected $l10n;
 
     public function __construct($template) {
+        $l10n = L10n::get('mustache');
         $this->mustache = new Mustache_Engine(array(
+            'cache' => __DIR__.'/../cache',
             'loader' => new Mustache_Loader_FilesystemLoader(dirname(dirname(__FILE__)).'/static/tpl'),
             'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(dirname(__FILE__)).'/static/tpl/partials'),
+            'helpers' => array('_' => function ($s) use ($l10n) {
+                return $l10n->gettext($s);
+            }),
+            'strict_callables' => true,
         ));
         $this->mustache->loadTemplate($template);
-        $this->l10n = L10n::get('mustache');
     }
 
     public function render($view = array()) {
-        $view['_'] = array($this, '_translate');
         return $this->mustache->render($view);
-    }
-
-    public function _translate($s) {
-        return $this->l10n->gettext($s);
     }
 
 }
