2b0f76f5e3380f1ad8172c21ae21e91d2ac42363
Manuel Strehl <git@manuel-strehl.de>
2013-06-01 22:27:44 +0200

    enhance scratchpad

--- src/js/components/scratchpad.js
+++ src/js/components/scratchpad.js
@@ -9,18 +9,43 @@ define([
 
   var _ = gettext.gettext;
 
+  /**
+   * at the moment we don't provide a fallback for older
+   * browsers or server-side syncing
+   */
   if ('localStorage' in window) {
 
     var scratchpad = JSON.parse(localStorage.getItem('scratchpad')),
+        scratchNode = $('<div class="scratchpad__container"></div>'),
+        scratchCtrl = $('<div class="scratchpad__controls"></div>').appendTo(scratchNode),
         sp_max_length = 128;
 
     if (! $.isArray(scratchpad)) {
       scratchpad = [];
     }
 
+    $('<button type="button">'+_('empty scratchpad')+'</button>').on('click',
+        function() {
+          scratchpad = [];
+          scratchNode.update();
+        }).appendTo(scratchCtrl);
+
+    scratchNode.update = function() {
+      this.find('.data, .quiet').remove();
+      if (scratchpad.length) {
+        var ul = this.prepend('<ul class="data"></ul>').find('.data');
+        $.each(scratchpad, function(i, v) {
+          ul.append('<li><a class="cp" href="http://codepoints.net/U+'+v.toString(16).toUpperCase()+'">'+v.toString(16).toUpperCase()+'</a></li>');
+        });
+      } else {
+        this.prepend('<p class="quiet">'+_('You have no codepoints here yet. Add one by clicking “Add to scratchpad” on the details page.')+'</p>');
+      }
+      return this;
+    }.bind(scratchNode);
+
     var opener = $('<li class="scratchpad"><a href="#">Scratchpad</a></li>')
                   .on('click', function() {
-                    $('<div>').text(cp(scratchpad)).dialog({
+                    scratchNode.update().dialog({
                       title: _('Scratchpad')
                     });
                     return false;
@@ -38,6 +63,7 @@ define([
           scratchpad.push(
             parseInt($('.representations .repr-number').text(), 10)
           );
+          scratchNode.update();
         });
       toolcontainer.append(btn);
     }
