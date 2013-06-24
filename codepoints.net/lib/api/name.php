b3a6eea4d28206baedc4bffcaa70fa25538fc482
Manuel Strehl <git@manuel-strehl.de>
2013-06-25 00:09:13 +0200

    return JSON for requesting clients on api/name

--- codepoints.net/lib/api/name.php
+++ codepoints.net/lib/api/name.php
@@ -2,7 +2,11 @@
 
 require_once __DIR__.'/../tools.php';
 
-$api->_mime = 'text/plain';
+if (isset($_SERVER['HTTP_ACCEPT']) &&
+    strpos($_SERVER['HTTP_ACCEPT'], 'application/json') === false) {
+    /* for non-JSON requests return plain text */
+    $api->_mime = 'text/plain';
+}
 
 if (maybeCodepoint($data) === false) {
     $api->throwError(API_BAD_REQUEST, _('No codepoint.'));
