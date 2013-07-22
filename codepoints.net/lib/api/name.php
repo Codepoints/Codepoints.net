2e629a1f8d46cd5301d2a1cbf80ba7bd41cc1c32
Manuel Strehl <com.github@manuel-strehl.de>
2013-07-22 16:41:38 +0200

    Return JSON-P, if callback is given (API-name)

--- codepoints.net/lib/api/name.php
+++ codepoints.net/lib/api/name.php
@@ -3,7 +3,8 @@
 require_once __DIR__.'/../tools.php';
 
 if (isset($_SERVER['HTTP_ACCEPT']) &&
-    strpos($_SERVER['HTTP_ACCEPT'], 'application/json') === false) {
+    strpos($_SERVER['HTTP_ACCEPT'], 'application/json') === false &&
+    ! array_key_exists('callback', $_GET)) {
     /* for non-JSON requests return plain text */
     $api->_mime = 'text/plain';
 }
