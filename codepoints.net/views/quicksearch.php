41772062ef2be076be9058bc6c3c9e6fa5ee8282
Manuel Strehl <git@manuel-strehl.de>
2013-12-16 02:07:24 +0100

    lose unnecessary label for quicksearch form

--- codepoints.net/views/quicksearch.php
+++ codepoints.net/views/quicksearch.php
@@ -1,6 +1,5 @@
 <?php if (! isset($searchprefix)) { $searchprefix = 'qs_'; }?>
 <form method="get" action="<?php e($router->getUrl('SearchResult'))?>" class="searchform">
-<p><label for="<?php e($searchprefix)?>q"><?php _e('Name:')?></label>
-  <input type="text" name="q" id="<?php e($searchprefix)?>q" value="<?php echo _get('q')?>" /></p>
-  <p><button type="submit"><span><?php _e('search')?></span></button></p>
+  <p><input type="text" name="q" id="<?php e($searchprefix)?>q" value="<?php echo _get('q')?>">
+  <button type="submit"><span><?php _e('search')?></span></button></p>
 </form>
