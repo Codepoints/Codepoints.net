c6a2d7aedd881789ec7105e8954d437b8c32f432
Manuel Strehl <git@manuel-strehl.de>
2014-03-01 00:52:19 +0100

    complete font mangler
    TODO: make it productive and add license and author infos

--- tools/codepoint_fonts.py
+++ tools/codepoint_fonts.py
@@ -3,6 +3,7 @@
 
 import fontforge
 import json
+import logging
 from   lxml import etree
 import os
 import os.path as op
@@ -11,9 +12,13 @@ import subprocess
 import sys
 
 
+logger = logging.getLogger('codepoint.fonts')
+
+
 ffGlyphXPath = etree.XPath('//glyph[string-length(@unicode) = 1]')
 xmlns_svg = 'http://www.w.3.org/2000/svg'
 glyphXPath = etree.XPath('//svg:glyph', namespaces={ 'svg': xmlns_svg })
+fontXPath = etree.XPath('//svg:font', namespaces={ 'svg': xmlns_svg })
 
 
 svg_font_skeleton = '''<svg xmlns="'''+xmlns_svg+'''" version="1.1">
@@ -60,18 +65,22 @@ def getBlocks():
     """get all blocks and their codepoints"""
     conn = sqlite3.connect('../ucd.sqlite')
     cur = conn.cursor()
+
+    # init blocks as dict keys
     blocks = {
-        x[0]: []
+        x[0]: {}
         for x in
             cur.execute('SELECT DISTINCT blk FROM codepoints;').fetchall() }
+
     for block in blocks.keys():
-        blocks[block].append(
-            [ x[0] for x in
-                cur.execute('SELECT cp FROM codepoints WHERE blk = ?', (block, ))
-                   .fetchall()
-            ])
-        blocks[block].append(etree.XML(svg_font_skeleton % (block, block)))
-        blocks[block].append("")
+        blocks[block]["cps"] = [ x[0] for x in
+            cur.execute('SELECT cp FROM codepoints WHERE blk = ?', (block, ))
+               .fetchall()
+        ]
+        blocks[block]["svgfont"] = etree.XML(svg_font_skeleton % (block, block))
+        blocks[block]["svgfontel"] = fontXPath(blocks[block]["svgfont"])[0]
+        blocks[block]["sql"] = ""
+
     return blocks
 
 
@@ -81,10 +90,10 @@ def distributeGlyphs(glyphs, blocks):
         cp = glyph.get("unicode")
         cpn = ord(cp)
         for block, block_data in blocks.iteritems():
-            if cpn in block_data[0]:
-                block_data[0].remove(cpn)
-                block_data[1].append(glyph)
-                block_data[2] += sql_tpl % (cpn, glyph.get('font-family'), block)
+            if cpn in block_data["cps"]:
+                block_data["cps"].remove(cpn)
+                block_data["svgfontel"].append(glyph)
+                block_data["sql"] += sql_tpl % (cpn, glyph.get('font-family'), block)
                 break
 
 
@@ -92,11 +101,13 @@ def main():
     blocks = getBlocks()
     fonts = getFonts()
     for item in fonts:
+        logger.info("Handling font {}".format(item[0]))
         glyphs = createSVGFont(item)
         distributeGlyphs(glyphs, blocks)
     for block, block_data in blocks.iteritems():
+        logger.info("Handling block {}".format(block))
         with open('blockfonts/'+block+'.svg', 'w') as svgfile:
-            svgfile.write(etree.tostring(block_data[1]))
+            svgfile.write(etree.tostring(block_data["svgfont"]))
         font = fontforge.open('blockfonts/'+block+'.svg')
         font.generate('blockfonts/'+block+'.woff')
         font.generate('blockfonts/'+block+'.ttf')
@@ -107,10 +118,11 @@ def main():
                 'blockfonts/'+block+'.eot',
         ])
         with open('blockfont_sql/'+block+'.sql', 'w') as sqlfile:
-            sqlfile.write(block_data[2])
+            sqlfile.write(block_data["sql"])
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, handler=logging.StreamHandler())
     main()
 
 #EOF
