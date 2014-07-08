2f9b6315e5743c4b94988c553bcf031f6e79d08c
Manuel Strehl <git@manuel-strehl.de>
2014-07-08 23:05:41 +0200

    bump Unicode version 7.0.0

--- tools/patch_db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS codepoint_fonts (
-    cp      INTEGER(7) REFERENCES codepoints,
-    font    TEXT,
-    id      TEXT, -- the font ID used as filename
-    PRIMARY KEY (cp, font)
-);
