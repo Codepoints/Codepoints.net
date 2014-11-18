9e98e4f8bda1e2aa2d43bf2f5fd62f6568489cf2
Manuel Strehl <m.strehl@kinetiqa.de>
2014-11-18 12:05:31 +0100

    fix #38: weight random numbers at expense of CJK and Hangul cps
    Every cp has still the chance to be selected, but for CJK and Hangul we take in
    account their sheer mass.

--- codepoints.net/controllers/random.php
+++ codepoints.net/controllers/random.php
@@ -2,7 +2,33 @@
 
 $router->registerAction('random', function ($request, $o) {
     // random codepoint
-    $x = $o['db']->prepare('SELECT cp FROM codepoints ORDER BY RANDOM() LIMIT 1');
+    $x = $o['db']->prepare('
+        SELECT cp
+            FROM codepoints
+            ORDER BY (
+                ABS(RANDOM())/10000000000000 * (
+                    CASE
+                    WHEN
+                        -- U+20000..U+2FFFF CJK in 3rd plane
+                        131072 <= cp AND cp <= 196607
+                    THEN
+                        2000000
+                    WHEN
+                        -- U+3400..U+4DBF CJK in BMP
+                        13312 <= cp AND cp <= 19903
+                    THEN
+                        1000000
+                    WHEN
+                        -- U+AC00..U+D7AF Hangul
+                        44032 <= cp AND cp <= 55215
+                    THEN
+                        500000
+                    ELSE
+                        1
+                    END
+                )
+            ) ASC
+            LIMIT 1');
     $x->execute();
     $row = $x->fetch();
     $router = Router::getRouter();
