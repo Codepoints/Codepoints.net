b4a4be6efdc73f852f709c8b293f2af1a2ef2c3e
Manuel Strehl <git@manuel-strehl.de>
2015-02-27 22:10:35 +0100

    switch API search to SearchComposer

--- codepoints.net/lib/api/search.php
+++ codepoints.net/lib/api/search.php
@@ -14,64 +14,8 @@ if (! count($_GET)) {
     );
 }
 
-$result = new SearchResult(array(), $api->_db);
-$cats = array_merge(UnicodeInfo::get()->getCategoryKeys(), array('int'));
-foreach ($_GET as $k => $v) {
-    if ($k === 'q' && $v) {
-        // "q" is a special case: We parse the query and try to
-        // figure, what's searched
-        if (mb_strlen($v, 'UTF-8') === 1) {
-            // seems to be one single character
-            $result->addQuery('cp', unpack('N', mb_convert_encoding($v,
-                                    'UCS-4BE', 'UTF-8')));
-        } else {
-            foreach (preg_split('/\s+/', $v) as $vv) {
-                if (ctype_xdigit($vv) && in_array(strlen($vv), array(4,5,6))) {
-                    $result->addQuery('cp', hexdec($vv), '=', 'OR');
-                }
-                if (substr(strtolower($vv), 0, 2) === 'u+' &&
-                    ctype_xdigit(substr($vv, 2))) {
-                    $result->addQuery('cp', hexdec(substr($vv, 2)), '=', 'OR');
-                }
-                if (ctype_digit($vv) && strlen($vv) < 8) {
-                    $result->addQuery('cp', intval($vv), '=', 'OR');
-                }
-                $result->addQuery('na', $vv, 'LIKE', 'OR');
-                $result->addQuery('na1', $vv, 'LIKE', 'OR');
-                $vv = "%$vv%";
-                $result->addQuery('kDefinition', $vv, 'LIKE', 'OR');
-                $result->addQuery('alias', $vv, 'LIKE', 'OR');
-                $result->addQuery('abstract', $vv, 'LIKE', 'OR');
-                if (preg_match('/\blowercase\b/i', $vv)) {
-                    $result->addQuery('gc', 'lc', '=', 'OR');
-                }
-                if (preg_match('/\buppercase\b/i', $vv)) {
-                    $result->addQuery('gc', 'uc', '=', 'OR');
-                }
-                if (preg_match('/\btitlecase\b/i', $vv)) {
-                    $result->addQuery('gc', 'tc', '=', 'OR');
-                }
-            }
-        }
-    } elseif ($v && $k === 'scx') {
-        // scx is a list of sc's
-        $result->addQuery($k, $v);
-        $v2 = explode(' ', $v);
-        foreach($v2 as $v3) {
-            $result->addQuery($k, "%$v3%", 'LIKE', 'OR');
-        }
-    } elseif ($k === 'int' && $v !== "") {
-        $v = preg_split('/\s+/', $v);
-        foreach($v as $v2) {
-            if (ctype_digit($v2)) {
-                $result->addQuery($k, $v2, '=', 'OR');
-            }
-        }
-    } elseif ($v && in_array($k, $cats)) {
-        $result->addQuery($k, $v);
-    }
-    // else: that's an unrecognized GET param. Ignore it.
-}
+$q = new SearchComposer($_GET, $api->_db);
+$result = $q->getSearchResult();
 
 $page = isset($_GET['page'])? intval($_GET['page']) : 1;
 $limit = isset($_GET['per_page'])? min(1000, intval($_GET['per_page'])) : 1000;
