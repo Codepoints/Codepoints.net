dcab8a593e6f2d6aa81a1b980a464c41f68cf04f
Manuel Strehl <m.strehl@kinetiqa.de>
2015-12-15 09:42:36 +0100

    update i18n and API problem URL

--- codepoints.net/lib/api/usage.php
+++ codepoints.net/lib/api/usage.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../tools.php';
 $host = get_origin().'api/v1';
 
 return array(
-    "description" => _("Welcome to codepoint.net’s Unicode API. Most requests are JSON-based. The API supports for those requests JSONP via the `callback` GET parameter. The `glyph` and `property` methods return PNG images. When problems appear, the API response conforms to <http://tools.ietf.org/html/draft-nottingham-http-problem-04>."),
+    "description" => _("Welcome to codepoint.net’s Unicode API. Most requests are JSON-based. The API supports for those requests JSONP via the `callback` GET parameter. The `glyph` and `property` methods return PNG images. When problems appear, the API response conforms to <https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-02>. Documentation is published at <https://github.com/Codepoints/codepoints.net/wiki/API>."),
     "codepoint_url" => "$host/codepoint/{codepoint}{?property*}{?callback}",
     "block_url" => "$host/block/{block}{?callback}",
     "plane_url" => "$host/plane/{plane}{?callback}",
