2d30d2bc69b64de954a37648049c79ff39822c22
Manuel Strehl <git@manuel-strehl.de>
2021-02-17 08:34:43 +0100

    add PU code point properties

--- codepoints.net/lib/Unicode/CodepointInfo/Properties.php
+++ codepoints.net/lib/Unicode/CodepointInfo/Properties.php
@@ -12,11 +12,110 @@ use \Codepoints\Unicode\CodepointInfo;
  */
 class Properties extends CodepointInfo {
 
+    private Array $private_use_properties = [
+        'na' =>      '',
+        'JSN' =>     '',
+        'gc' =>      'Co',
+        'ccc' =>     '0',
+        'dt' =>      'none',
+        'nt' =>      'None',
+        'nv' =>      '',
+        'bc' =>      'L',
+        'Bidi_M' =>  0,
+        'bmg' =>     '',
+        'jt' =>      'U',
+        'jg' =>      'No_Joining_Group',
+        'ea' =>      'A',
+        'lb' =>      'XX',
+        'sc' =>      'Zzzz',
+        'Dash' =>    0,
+        'WSpace' =>  0,
+        'Hyphen' =>  0,
+        'QMark' =>   0,
+        'Radical' => 0,
+        'Ideo' =>    0,
+        'UIdeo' =>   0,
+        'IDSB' =>    0,
+        'IDST' =>    0,
+        'hst' =>     'NA',
+        'DI' =>      0,
+        'ODI' =>     0,
+        'Alpha' =>   0,
+        'OAlpha' =>  0,
+        'Upper' =>   0,
+        'OUpper' =>  0,
+        'Lower' =>   0,
+        'OLower' =>  0,
+        'Math' =>    0,
+        'OMath' =>   0,
+        'Hex' =>     0,
+        'AHex' =>    0,
+        'NChar' =>   0,
+        'VS' =>      0,
+        'Bidi_C' =>  0,
+        'Join_C' =>  0,
+        'Gr_Base' => 0,
+        'Gr_Ext' =>  0,
+        'OGr_Ext' => 0,
+        'Gr_Link' => 0,
+        'STerm' =>   0,
+        'Ext' =>     0,
+        'Term' =>    0,
+        'Dia' =>     0,
+        'Dep' =>     0,
+        'IDS' =>     0,
+        'OIDS' =>    0,
+        'XIDS' =>    0,
+        'IDC' =>     0,
+        'OIDC' =>    0,
+        'XIDC' =>    0,
+        'SD' =>      0,
+        'LOE' =>     0,
+        'Pat_WS' =>  0,
+        'Pat_Syn' => 0,
+        'GCB' =>     'XX',
+        'WB' =>      'XX',
+        'SB' =>      'XX',
+        'CE' =>      0,
+        'Comp_Ex' => 0,
+        'NFC_QC' =>  1,
+        'NFD_QC' =>  1,
+        'NFKC_QC' => 1,
+        'NFKD_QC' => 1,
+        'XO_NFC' =>  0,
+        'XO_NFD' =>  0,
+        'XO_NFKC' => 0,
+        'XO_NFKD' => 0,
+        'FC_NFKC' => '',
+        'CI' =>      0,
+        'Cased' =>   0,
+        'CWCF' =>    0,
+        'CWCM' =>    0,
+        'CWKCF' =>   0,
+        'CWL' =>     0,
+        'CWT' =>     0,
+        'CWU' =>     0,
+        'isc' =>     '',
+        'na1' =>     '',
+    ];
+
+    private Array $private_use_self_references = [
+        'dm', 'suc', 'slc', 'stc', 'uc', 'lc', 'tc', 'scf', 'cf', 'NFKC_CF', ];
+
     /**
      * return the official Unicode properties for a code point
      */
     public function __invoke(Codepoint $codepoint, Array $args) : array {
         $properties = [];
+
+        if (is_pua($codepoint->id)) {
+            $properties = $this->private_use_properties;
+            $properties['age'] = ($codepoint->id <= 0xF8FF? '1.1' : '2.0');
+            foreach ($this->private_use_self_references as $prop) {
+                $properties[$prop] = $codepoint;
+            }
+            return $properties;
+        }
 
         $data = $this->db->getOne('
             SELECT props.*, script.sc AS script
