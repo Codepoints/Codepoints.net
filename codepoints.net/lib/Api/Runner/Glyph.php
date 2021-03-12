5d354e504213c07569ff89ad1676400e5896861a
Manuel Strehl <git@manuel-strehl.de>
2021-03-12 01:41:39 +0100

    add glyph api endpoint

---
+++ codepoints.net/lib/Api/Runner/Glyph.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Codepoints\Api\Runner;
+
+use Codepoints\Api\Runner;
+use Codepoints\Api\Exception as ApiException;
+
+
+class Glyph extends Runner {
+
+    /**
+     * return an example glyph for the requested code point
+     */
+    public function handle(string $data) : string {
+        if (! $data || strlen($data) > 6 || ctype_xdigit($data) === false) {
+            throw new ApiException(__('No codepoint'), ApiException::BAD_REQUEST);
+        }
+
+        $cp = get_codepoint(hexdec($data), $this->env['db']);
+        if (! $cp) {
+            throw new ApiException(__('Not a codepoint'), ApiException::NOT_FOUND);
+        }
+
+        $img = $this->env['db']->getOne('
+            SELECT image
+            FROM codepoint_image
+            WHERE cp = ?
+            LIMIT 1', $cp->id);
+
+        if ($img) {
+            if (! DEBUG) {
+                /* extend caching to 1 week */
+                header('Cache-Control: public, mag-age=604800');
+                header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 604800) . ' GMT');
+            }
+
+            header('Content-Type: image/svg+xml');
+            if (isset($_SERVER['HTTP_ACCEPT_ENCODING']) &&
+                strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== false) {
+                header('Content-Encoding: gzip');
+                return gzencode($img['image']);
+            } else {
+                return $img['image'];
+            }
+        } else {
+            throw new ApiException(__('No image found'), ApiException::NOT_FOUND);
+        }
+    }
+
+}
