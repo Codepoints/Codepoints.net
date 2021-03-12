4c63818985c63ab72beb13307be16716ad60d3a8
Manuel Strehl <git@manuel-strehl.de>
2021-03-12 01:46:58 +0100

    gettext correction

--- codepoints.net/lib/Api/Runner/Problem.php
+++ codepoints.net/lib/Api/Runner/Problem.php
@@ -10,24 +10,24 @@ class Problem extends JsonRunner {
     protected function handle_request(string $data) : Array {
         switch ($data) {
             case 'precondition_failed':
-                $name = _('Prerequisite Missing');
-                $info = sprintf(_('A prerequisite was not found. That means, that the action to be called does not exist. See %s/ for API usage.'), 'https://codepoints.net/api/v1');
+                $name = __('Prerequisite Missing');
+                $info = sprintf(__('A prerequisite was not found. That means, that the action to be called does not exist. See %s/ for API usage.'), 'https://codepoints.net/api/v1');
                 break;
             case 'request_uri_too_long':
-                $name = _('Request Too Long');
-                $info = _('The request body (or the data in the URL) was too large. Try to reduce it.');
+                $name = __('Request Too Long');
+                $info = __('The request body (or the data in the URL) was too large. Try to reduce it.');
                 break;
             case 'bad_request':
-                $name = _('Bad Request');
-                $info = sprintf(_('The client made a bad request, for example, by not specifying a required parameter. See %s/ for API usage.'), 'https://codepoints.net/api/v1');
+                $name = __('Bad Request');
+                $info = sprintf(__('The client made a bad request, for example, by not specifying a required parameter. See %s/ for API usage.'), 'https://codepoints.net/api/v1');
                 break;
             case 'not_found':
-                $name = _('Not Found');
-                $info = _('The requested resource was not found. This can, for example, happen for non-existing codepoints.');
+                $name = __('Not Found');
+                $info = __('The requested resource was not found. This can, for example, happen for non-existing codepoints.');
                 break;
             default:
-                $name = _('Error');
-                $info = _('An unspecific error occurred.');
+                $name = __('Error');
+                $info = __('An unspecific error occurred.');
                 break;
         }
 
