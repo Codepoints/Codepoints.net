a509cce67a78a3517d3867830073dc59f959b0e5
Manuel Strehl <git@manuel-strehl.de>
2021-03-18 17:47:05 +0100

    extend tests in astral planes

--- tests/acceptance/CodepointCept.php
+++ tests/acceptance/CodepointCept.php
@@ -3,20 +3,22 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('check codepoint pages');
 
 $categories = [
-    'ascii' =>     ['U+004F',   200], # LATIN CAPITAL LETTER O
     'control' =>   ['U+0000',   200],
-    'nonchar' =>   ['U+3FFFF',  200],
-    'reserved' =>  ['U+FFF4',   404],
-    'emoji' =>     ['U+1F6E3',  200], # MOTORWAY
-    'plane2' =>    ['U+20000',  200],
-    'last' =>      ['U+10FFFF', 200],
-    'beyond' =>    ['U+1123AF', 404], # Something beyond the last code point
+    'ascii' =>     ['U+004F',   200], # LATIN CAPITAL LETTER O
     'arabic' =>    ['U+0636',   200], # ARABIC LETTER DAD
+    'rtl' =>       ['U+202E',   200], # RIGHT-TO-LEFT OVERRIDE
     'cjk' =>       ['U+4E15',   200], # CJK UNIFIED IDEOGRAPH-4E15
     'modifier' =>  ['U+A706',   200], # MODIFIER LETTER CHINESE TONE YIN RU
     'surrogate' => ['U+D801',   200],
-    'pua' =>       ['U+E123',   200],
-    'rtl' =>       ['U+202E',   200], # RIGHT-TO-LEFT OVERRIDE
+    'pua' =>       ['U+E123',   200], # private-use code point
+    'reserved' =>  ['U+FFF4',   404],
+    'emoji' =>     ['U+1F6E3',  200], # MOTORWAY
+    'plane2' =>    ['U+20000',  200], # existing 3rd plane character
+    'nonchar' =>   ['U+3FFFF',  200], # non-character
+    'unas_astr' => ['U+69876A', 404], # Astral unassigned code point
+    'pua_astr' =>  ['U+10456A', 200], # Astral private-use code point
+    'last' =>      ['U+10FFFF', 200],
+    'beyond' =>    ['U+1123AF', 404], # Something beyond the last code point
 ];
 
 foreach ($categories as $category => list($codepoint, $response_code)) {
