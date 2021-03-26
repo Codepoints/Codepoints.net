1d814e95fae0c3d20f60157a73ecc752b053c5bd
Manuel Strehl <git@manuel-strehl.de>
2021-03-26 08:04:11 +0100

    add icons to header and footer
    We leave FontAwesome, because there are lots of suitable
    "plain text" icons in Unicode already.

--- codepoints.net/views/partials/footer.php
+++ codepoints.net/views/partials/footer.php
@@ -6,9 +6,15 @@
           '<a rel="nofollow" href="https://github.com/Codepoints/codepoints.net">'.__('Feedback and contributions').'</a>')?></p>
       <nav>
         <ul>
-          <li><a href="https://twitter.com/CodepointsNet"><?=_q('Twitter')?></a></li>
-          <li><a href="https://blog.codepoints.net/"><?=_q('Blog')?></a></li>
-          <li><a href="<?=q(url('about'))?>#this_site"><?=_q('About')?></a></li>
+          <li><a href="https://twitter.com/CodepointsNet">
+            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F426#U1F426"/></svg>
+            <?=_q('Twitter')?></a></li>
+          <li><a href="https://blog.codepoints.net/">
+            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F56E#U1F56E"/></svg>
+            <?=_q('Blog')?></a></li>
+          <li><a href="<?=q(url('about'))?>#this_site">
+            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F6C8#U1F6C8"/></svg>
+            <?=_q('About')?></a></li>
         </ul>
       </nav>
     </footer>
--- codepoints.net/views/partials/main-navigation.php
+++ codepoints.net/views/partials/main-navigation.php
@@ -1,10 +1,20 @@
 <nav class="main-nav">
   <ul class="main-nav__primary">
-    <li class="start"><a href="<?=q(url(''))?>" rel="start"><?=_q('Codepoints')?></a></li>
-    <li class="search"><a href="<?=q(url('search'))?>" rel="search"><?=_q('Search')?></a></li>
-    <li class="scripts"><a href="<?=q(url('scripts'))?>"><?=_q('Scripts')?></a></li>
-    <li class="random"><a rel="nofollow" href="<?=q(url('random'))?>"><?=_q('Random')?></a></li>
-    <li class="about"><a rel="nofollow" href="<?=q(url('about'))?>"><?=_q('About')?></a></li>
+    <li class="start"><a href="<?=q(url(''))?>" rel="start">
+      <img src="/static/images/icon.svg" width="16" height="16">
+      <?=_q('Codepoints')?></a></li>
+    <li class="search"><a href="<?=q(url('search'))?>" rel="search">
+      <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F50E#U1F50E"/></svg>
+      <?=_q('Search')?></a></li>
+    <li class="scripts"><a href="<?=q(url('scripts'))?>">
+      <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F310#U1F310"/></svg>
+      <?=_q('Scripts')?></a></li>
+    <li class="random"><a rel="nofollow" href="<?=q(url('random'))?>">
+      <svg width="16" height="16"><use xlink:href="/api/v1/glyph/27F3#U27F3"/></svg>
+      <?=_q('Random')?></a></li>
+    <li class="about"><a rel="nofollow" href="<?=q(url('about'))?>">
+      <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F6C8#U1F6C8"/></svg>
+      <?=_q('About')?></a></li>
   </ul>
   <?php
 
