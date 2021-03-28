4aee968d885924ffb718da0e783beee370f6088f
Manuel Strehl <git@manuel-strehl.de>
2021-03-28 22:14:13 +0200

    show HTML API docs, if Accept header allows

--- codepoints.net/lib/Api/Runner/Usage.php
+++ codepoints.net/lib/Api/Runner/Usage.php
@@ -2,15 +2,22 @@
 
 namespace Codepoints\Api\Runner;
 
-use Codepoints\Api\JsonRunner;
+use Codepoints\Api\Runner;
+use Codepoints\View;
 
 
-class Usage extends JsonRunner {
+class Usage extends Runner {
 
-    protected function handle_request(string $data) : Array {
+    public function handle(string $data) : string {
+        if (isset($_SERVER['HTTP_ACCEPT']) &&
+            strpos($_SERVER['HTTP_ACCEPT'], 'text/html') !== false) {
+            return file_get_contents(dirname(dirname(dirname(__DIR__))).'/views/api.html');
+        }
+
         $host = 'https://codepoints.net/api/v1';
 
-        return [
+        header('Content-Type: application/json; charset=utf-8');
+        return json_encode([
             'description' => __('Welcome to codepoint.net’s Unicode API. Most requests are JSON-based. The API supports for those requests JSONP via the `callback` GET parameter. The `glyph` and `property` methods return PNG images. When problems appear, the API response conforms to <https://tools.ietf.org/html/draft-ietf-appsawg-http-problem-02>. Documentation is published at <https://github.com/Codepoints/codepoints.net/wiki/API>.'),
             'codepoint_url' => "$host/codepoint/{codepoint}{?property*}{?callback}",
             'block_url' => "$host/block/{block}{?callback}",
@@ -28,7 +35,7 @@ class Usage extends JsonRunner {
                 'iso' => '[A-Z][a-z]{3}',
                 'page' => '[1-9][0-9]*',
             ],
-        ];
+        ]);
     }
 
 }
