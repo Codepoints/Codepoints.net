6965b94094e175a4c73aed882ee0205a917081b5
Manuel Strehl <git@manuel-strehl.de>
2021-04-13 10:20:47 +0200

    add description to offline.html

--- codepoints.net/views/offline.php
+++ codepoints.net/views/offline.php
@@ -2,5 +2,6 @@
 <main class="main main--offline">
   <h1><?=q($title)?></h1>
   <p><?=q($page_description)?></p>
+  <p><?=_q('The home page, the plane overview and the detail pages for the seventeen planes should also be available.')?></p>
 </main>
 <?php include 'partials/footer.php'; ?>
