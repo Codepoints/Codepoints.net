8495b69c5f66a5c7b46590ca3c92cfe9ed2bc23e
Manuel Strehl <git@manuel-strehl.de>
2022-02-22 22:31:04 +0100

    change the way images are shown
    Compare unicode2mysql changes.

--- codepoints.net/lib/Api/Runner/Glyph.php
+++ codepoints.net/lib/Api/Runner/Glyph.php
@@ -34,6 +34,9 @@ class Glyph extends Runner {
                 header('Expires: ' . gmdate('D, d M Y H:i:s', time() + 604800) . ' GMT');
             }
             header('Content-Type: image/svg+xml');
+            if (strpos($img['image'], 'xmlns="http://www.w3.org/2000/svg"') === false) {
+                $img['image'] = str_replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ', $img['image']);
+            }
             return $img['image'];
         } else {
             throw new ApiException(__('No image found'), ApiException::NOT_FOUND);
