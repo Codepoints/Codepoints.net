ba31d0b9cc1695f704731f82734d8c31c87abc7a
Manuel Strehl <git@manuel-strehl.de>
2022-07-16 18:44:04 +0200

    add dark mode support for favicon

--- codepoints.net/views/partials/form-darkmode.php
+++ codepoints.net/views/partials/form-darkmode.php
@@ -15,18 +15,33 @@ chk.addEventListener('click', function() {
         chk.indeterminate = false;
         root.classList.replace('force-dark', 'force-light');
         document.cookie = 'force_mode=light';
+        updateIcon('light');
     } else if (root.classList.contains('force-light')) {
         chk.checked = false;
         chk.indeterminate = true;
         root.classList.remove('force-light');
         document.cookie = 'force_mode=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+        updateIcon();
     } else {
         chk.checked = true;
         chk.indeterminate = false;
         root.classList.add('force-dark');
         document.cookie = 'force_mode=dark';
+        updateIcon('dark');
     }
 });
+updateIcon();
+
+function updateIcon(to) {
+    if (! to) {
+        to = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)? 'dark' : 'light';
+    }
+    var old_url = to === 'light'? '/static/images/icon.dark.svg' : '/static/images/icon.svg';
+    var new_url = to === 'light'? '/static/images/icon.svg' : '/static/images/icon.dark.svg';
+    document.querySelectorAll('img[src="'+old_url+'"]').forEach(function(img) {
+        img.src = new_url;
+    });
+}
 })();
 </script>
 </p>
