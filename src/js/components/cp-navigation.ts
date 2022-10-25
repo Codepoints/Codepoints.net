8832a248ae1d3b5ad50836f21eb951acb837f6d4
Manuel Strehl <git@manuel-strehl.de>
2022-10-25 21:23:39 +0200

    embed SVG icons better in <cp-navigation>

--- src/js/components/cp-navigation.ts
+++ src/js/components/cp-navigation.ts
@@ -1,7 +1,10 @@
 import { LitElement, css, unsafeCSS, html } from 'lit';
+import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
 import { customElement, property } from 'lit/decorators.js';
 import { gettext as _ } from '../_i18n.ts';
 import { customMedia } from '../media_queries.ts';
+import IconMagnifyingGlass from '@fortawesome/fontawesome-free/svgs/solid/magnifying-glass.svg?raw';
+import IconShuffle from '@fortawesome/fontawesome-free/svgs/solid/shuffle.svg?raw';
 
 @customElement('cp-navigation')
 export class CpNavigation extends LitElement {
@@ -43,9 +46,6 @@ export class CpNavigation extends LitElement {
     }
   `;
 
-  /**
-   * icons by font awesome
-   */
   render() {
     const home = html`<a href="/">
       <span class="meta">${_('Home')}</span></a>`;
@@ -53,10 +53,10 @@ export class CpNavigation extends LitElement {
       <svg width="16" height="16"><svg viewBox="194 97 1960 1960" width="100%" height="100%"><use xlink:href="/static/images/unicode-logo-framed.svg#unicode" width="16" height="16"/></svg></svg>
       <span class="meta">${_('Code Points')}</span></a>`;
     const up = html`<a href="/search">
-      <svg viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
+      ${unsafeSVG(IconMagnifyingGlass)}
       <span class="meta">${_('Search')}</span></a>`;
     const next = html`<a href="/random">
-      <svg viewBox="0 0 512 512"><path d="M403.8 34.4c12-5 25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V160H352c-10.1 0-19.6 4.7-25.6 12.8L284 229.3 244 176l31.2-41.6C293.3 110.2 321.8 96 352 96h32V64c0-12.9 7.8-24.6 19.8-29.6zM164 282.7L204 336l-31.2 41.6C154.7 401.8 126.2 416 96 416H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c10.1 0 19.6-4.7 25.6-12.8L164 282.7zm274.6 188c-9.2 9.2-22.9 11.9-34.9 6.9s-19.8-16.6-19.8-29.6V416H352c-30.2 0-58.7-14.2-76.8-38.4L121.6 172.8c-6-8.1-15.5-12.8-25.6-12.8H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H96c30.2 0 58.7 14.2 76.8 38.4L326.4 339.2c6 8.1 15.5 12.8 25.6 12.8h32V320c0-12.9 7.8-24.6 19.8-29.6s25.7-2.2 34.9 6.9l64 64c6 6 9.4 14.1 9.4 22.6s-3.4 16.6-9.4 22.6l-64 64z"/></svg>
+      ${unsafeSVG(IconShuffle)}
       <span class="meta">${_('Random')}</span></a>`;
     return html`
     <nav>
