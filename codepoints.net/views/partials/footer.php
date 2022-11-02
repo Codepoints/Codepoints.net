1aaa5d3446d930a91d2a9c1cb9eed54237e9848e
Manuel Strehl <git@manuel-strehl.de>
2022-11-02 19:47:05 +0100

    re-structure footer and mention current Unicode version
    Thanks to @hsivonen for this suggestion!

--- codepoints.net/views/partials/footer.php
+++ codepoints.net/views/partials/footer.php
@@ -1,34 +1,25 @@
       </div>
     </div>
     <footer class="page-footer">
-      <p><?php printf(__('This is Codepoints.net, a site dedicated to all things characters, letters and Unicode. The site is run by %s. The content can be %s under the given terms. We’d like to thank %s for making Codepoints.net possible. %s are always welcome.'),
-          '<a href="http://www.manuel-strehl.de">Manuel Strehl</a>',
-          '<a rel="nofollow" href="'.q(url('about')).'#this_site">'.__('freely reused').'</a>',
-          '<a rel="nofollow" href="'.q(url('about')).'#attribution">'.__('these fine people').'</a>',
-          '<a rel="nofollow" href="https://github.com/Codepoints/codepoints.net">'.__('Feedback and contributions').'</a>')?></p>
+      <p><?=_q('This is Codepoints.net, a site dedicated to all things characters, letters and Unicode.')?>
+         <?php printf(__('Currently displaying Unicode version %s.'), '<strong>'.UNICODE_VERSION.'</strong>')?>
+         <a href="<?=q(url('about'))?>"><?=_q('Read more about this site.')?></a></p>
       <nav>
         <ul>
           <li><a href="https://twitter.com/CodepointsNet">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F426#U1F426"/></svg>
             <?=_q('Twitter')?></a></li>
           <li><a href="https://blog.codepoints.net/">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F56E#U1F56E"/></svg>
             <?=_q('Blog')?></a></li>
-          <li class="search"><a href="<?=q(url('search'))?>" rel="search">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F50E#U1F50E"/></svg>
+          <li><a href="<?=q(url('search'))?>" rel="search">
             <?=_q('Search')?></a></li>
-          <li class="scripts"><a href="<?=q(url('scripts'))?>">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F310#U1F310"/></svg>
+          <li><a href="<?=q(url('scripts'))?>">
             <?=_q('Scripts')?></a></li>
-          <li class="random"><a rel="nofollow" href="<?=q(url('random'))?>">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/27F3#U27F3"/></svg>
+          <li><a rel="nofollow" href="<?=q(url('random'))?>">
             <?=_q('Random')?></a></li>
-          <li class="about"><a href="<?=q(url('about'))?>#this_site">
-            <svg width="16" height="16"><use xlink:href="/api/v1/glyph/1F6C8#U1F6C8"/></svg>
+          <li><a href="<?=q(url('about'))?>#this_site">
             <?=_q('About')?></a></li>
         </ul>
       </nav>
-      <?php include 'form-quicksearch.php' ?>
     </footer>
     <?php include 'tracker.php' ?>
     <?php include 'service_worker.php' ?>
