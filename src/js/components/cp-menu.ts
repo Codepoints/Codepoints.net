2148e134732287dfb04617d90a6da3c20da7a041
Manuel Strehl <git@manuel-strehl.de>
2022-11-05 23:04:07 +0100

    add possibility to close main menu and effect

--- src/js/components/cp-menu.ts
+++ src/js/components/cp-menu.ts
@@ -5,11 +5,45 @@ import { gettext as _ } from '../_i18n.ts';
 @customElement('cp-menu')
 export class CpMenu extends LitElement {
   static styles = css`
+  dialog {
+    margin: 0;
+    width: auto;
+    min-height: 50vh;
+    border: none;
+    padding: 2rem;
+  }
+  dialog[open] {
+    animation-name: menu;
+    animation-duration: .5s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease-out;
+  }
+  @keyframes menu {
+    from {
+      top: -20vh;
+      opacity: .333;
+    }
+    to {
+      top: 0;
+      opacity: 1;
+    }
+  }
+  @media (prefers-reduced-motion: reduce) {
+    @keyframes menu {
+      from {
+        opacity: .5;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  }
   `;
 
   render() {
     return html`
     <dialog>
+      <button type="button" @click="${this.close}">${_('close')}</button>
       <a href="/">${_('go to the start page')}</a>
       <cp-darkmode></cp-darkmode>
       <cp-language></cp-language>
@@ -29,5 +63,15 @@ export class CpMenu extends LitElement {
 
   show() {
     this.renderRoot.querySelector('dialog').showModal();
+    this.animation = this.renderRoot.querySelector('dialog').getAnimations()?.[0];
+  }
+
+  close() {
+    if (this.animation) {
+      this.animation.reverse();
+      window.setTimeout(() => this.renderRoot.querySelector('dialog').close(), 500);
+    } else {
+      this.renderRoot.querySelector('dialog').close();
+    }
   }
 }
