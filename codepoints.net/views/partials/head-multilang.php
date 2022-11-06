0812706ce3cb115f7598b0271920eddca0eed6ec
Manuel Strehl <git@manuel-strehl.de>
2022-11-06 16:44:37 +0100

    fix double lang params in URL

--- codepoints.net/views/partials/head-multilang.php
+++ codepoints.net/views/partials/head-multilang.php
@@ -5,10 +5,11 @@ use Codepoints\Translator;
  * @var Array $env
  */
 ?>
-<?php if (array_key_exists('current_url', $env)): ?>
-  <?php foreach (Translator::SUPPORTED_LANGUAGES as $other_lang): ?>
-    <?php /* Google says, that it wants a link element to the same language, too:
-           * https://developers.google.com/search/docs/advanced/crawling/localized-versions */ ?>
-    <link rel="alternate" hreflang="<?=q($other_lang)?>" title="<?=q(Translator::getLanguageName($other_lang))?>" href="https://codepoints.net/<?=$env['current_url'].(strpos($env['current_url'], '?') !== false? '&' : '?').'lang='.q($other_lang)?>">
-  <?php endforeach ?>
-<?php endif ?>
+<?php if (array_key_exists('current_url', $env)):
+    $url = preg_replace('/(\\?lang=[^&]*$|(?<=\\?)lang=[^&]*&|&lang=[^&]*)/', '', $env['current_url']);
+    foreach (Translator::SUPPORTED_LANGUAGES as $other_lang):
+        /* Google says, that it wants a link element to the same language, too:
+         * https://developers.google.com/search/docs/advanced/crawling/localized-versions */
+        ?><link rel="alternate" hreflang="<?=q($other_lang)?>" title="<?=q(Translator::getLanguageName($other_lang))?>" href="https://codepoints.net/<?=$url.(strpos($url, '?') !== false? '&' : '?').'lang='.q($other_lang)?>">
+  <?php endforeach;
+endif ?>
