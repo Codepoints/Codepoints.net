1ca7718aac8342b0da36a53f531b1f35ba3af73f
Manuel Strehl <git@manuel-strehl.de>
2022-11-25 22:01:58 +0100

    replace popular API endpoint with dedicated method

--- codepoints.net/lib/Api/Runner/Popular.php
+++ codepoints.net/lib/Api/Runner/Popular.php
@@ -8,6 +8,7 @@ use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Contracts\Cache\ItemInterface;
 use Codepoints\Api\JsonRunner;
 use Codepoints\Api\Exception as ApiException;
+use Codepoints\Unicode\Codepoint;
 
 
 class Popular extends JsonRunner {
@@ -23,40 +24,33 @@ class Popular extends JsonRunner {
     }
 
     private function fetch_fresh() : Array {
-        $most_popular = [];
-        $api_token = $this->env['config']['stats']['token'];
-        $date2 = new DateTimeImmutable();
-        $interval = new DateInterval('P2D');
-        $date1 = $date2->sub($interval);
-        if (! $date1) {
-            return $most_popular;
-        }
-        $params = [
-            'module' => 'API',
-            'format' => 'json',
-            'method' => 'Actions.getPageUrls',
-            'idSite' => '4',
-            'date' => sprintf('%s,%s', $date1->format('Y-m-d'), $date2->format('Y-m-d')),
-            'period' => 'range',
-            'filter_limit' => $this->count + 10, // we need only 20, but overfetch to cater for wrong matches
-            'showColumns' => 'label,url',
-            'filter_column' => 'label',
-            'filter_pattern' => '^/U',
-            'token_auth' => $api_token,
-        ];
-        $api_url = 'https://stats.codepoints.net/?' . http_build_query($params);
+        $intlist = [];
+        $api_url = 'https://stats.codepoints.net/popular.php';
         $data = json_decode(file_get_contents($api_url), true);
         if ($data) {
             foreach ($data as $item) {
                 if (preg_match('#^/U ([a-fA-F0-9]{4,6})(?:\?.*)?$#', $item['label'], $match)) {
-                    $most_popular[] = hexdec($match[1]);
-                    if (count($most_popular) >= $this->count) {
-                        break;
-                    }
+                    $intlist[] = hexdec($match[1]);
                 }
             }
         }
-        return $most_popular;
+        $cps = [];
+        $list = join(',', array_unique($intlist));
+        $data = $this->env['db']->getAll('SELECT cp, name, gc FROM codepoints
+            WHERE cp IN ( '.$list.' ) ORDER BY FIELD( cp, '.$list.' )');
+        if ($data) {
+            foreach ($data as $set) {
+                $cp = Codepoint::getCached($set, $this->env['db']);
+                $image_generator = $cp->image;
+                $cps[] = [
+                    $cp->id,
+                    $image_generator(250),
+                    $cp->name,
+                    $cp->gc
+                ];
+            }
+        }
+        return $cps;
     }
 
 }
