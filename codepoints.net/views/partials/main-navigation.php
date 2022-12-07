15f1985a73ed78baa2996ac67ba1d4db61d135fc
Manuel Strehl <git@manuel-strehl.de>
2022-12-07 23:35:43 +0100

    move composition of main nav back into HTML
    The web component is responsible for the interaction only

--- codepoints.net/views/partials/main-navigation.php
+++ codepoints.net/views/partials/main-navigation.php
@@ -1,13 +1,24 @@
+<?php
+/**
+ * @var ?array $nav
+ */
+?>
 <cp-navigation>
-  <a slot="home" href="<?=q(url(''))?>" rel="start" class="ln">
+  <a href="<?=q(url(''))?>" rel="start" class="ln">
     <svg width="16" height="16"><use href="/static/images/icon.svg#icon"/></svg>
     <span class="title"><?=_q('Home')?></span>
   </a>
 <?php
-/** @psalm-suppress RedundantCondition */
-if (isset($nav) && count($nav)):
-    if (array_key_exists('prev', $nav)) { echo str_replace('<a ', '<a slot="prev" ', $nav['prev']); }
-    if (array_key_exists('up', $nav)) { echo str_replace('<a ', '<a slot="up" ', $nav['up']); }
-    if (array_key_exists('next', $nav)) { echo str_replace('<a ', '<a slot="next" ', $nav['next']); }
-endif?>
+if (! isset($nav) || ! $nav) {
+  $nav = [];
+} ?>
+<?= array_key_exists('prev', $nav) ? $nav['prev'] : '<a href="/search">
+  <svg width="16" height="16"><use href="/static/images/magnifying-glass.svg#root"/></svg>
+  <span class="title">'._('Search').'</span></a>' ?>
+<?= array_key_exists('up', $nav) ? $nav['up'] : '<a href="/planes">
+  <svg width="16" height="16"><svg viewBox="194 97 1960 1960" width="100%" height="100%"><use href="'.static_url('images/unicode-logo-framed.svg#unicode').'" width="16" height="16"/></svg></svg>
+  <span class="title">'._('All Planes').'</span></a>' ?>
+<?= array_key_exists('next', $nav) ? $nav['next'] : '<a href="/random">
+  <svg width="16" height="16"><use href="'.static_url('images/shuffle.svg#root').'"/></svg>
+  <span class="title">'._('Random').'</span></a>' ?>
 </cp-navigation>
--- src/js/components/cp-navigation.ts
+++ src/js/components/cp-navigation.ts
@@ -1,65 +1,29 @@
-import { LitElement, css, html } from 'lit';
-import { unsafeSVG } from 'lit/directives/unsafe-svg.js';
+import { LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { gettext as _ } from '../_i18n.ts';
-import IconMagnifyingGlass from '@fortawesome/fontawesome-free/svgs/solid/magnifying-glass.svg?raw';
-import IconShuffle from '@fortawesome/fontawesome-free/svgs/solid/shuffle.svg?raw';
 import { CpMenu } from './cp-menu.ts';
 
 @customElement('cp-navigation')
 export class CpNavigation extends LitElement {
-  static styles = css`
-    :host > slot > * {
-      width: 25%;
-      flex-grow: 1;
-    }
-    a, button, ::slotted(a), ::slotted(button) {
-      display: block;
-      width: auto;
-      background: var(--color-back);
-      text-align: center;
-      color: var(--color-link);
-      text-decoration: none;
-    }
-    a svg {
-      display: block;
-      width: 80%;
-      height: 80%;
-      max-width: 64px;
-      max-height: 64px;
-      margin: 1em auto 0;
-      transition: transform .3s, color .3s;
-      vertical-align: top;
-      fill: currentColor;
-    }
-
-    a:focus > svg,
-    a:hover > svg {
-      transform: scale(1.33);
-    }
-  `;
-
   @property()
   declare menu: CpMenu;
 
+  connectedCallback() {
+    super.connectedCallback();
+    this.showMenu = this.showMenu.bind(this);
+    this.querySelector('[rel="start"]').addEventListener('click', this.showMenu);
+  }
+
+  disconnectedCallback() {
+    this.querySelector('[rel="start"]').removeEventListener('click', this.showMenu);
+    super.disconnectedCallback();
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+
   render() {
-    const home = html`<a href="/">
-      <span class="title">${_('Home')}</span></a>`;
-    const prev = html`<a href="/search">
-      ${unsafeSVG(IconMagnifyingGlass)}
-      <span class="title">${_('Search')}</span></a>`;
-    const up = html`<a href="/planes">
-      <svg width="16" height="16"><svg viewBox="194 97 1960 1960" width="100%" height="100%"><use xlink:href="/static/images/unicode-logo-framed.svg#unicode" width="16" height="16"/></svg></svg>
-      <span class="title">${_('All Planes')}</span></a>`;
-    const next = html`<a href="/random">
-      ${unsafeSVG(IconShuffle)}
-      <span class="title">${_('Random')}</span></a>`;
-    return html`
-      <slot name="home" @click="${this.showMenu}">${home}</slot>
-      <slot name="prev">${prev}</slot>
-      <slot name="up">${up}</slot>
-      <slot name="next">${next}</slot>
-    `;
+    return;
   }
 
   showMenu(event) {
