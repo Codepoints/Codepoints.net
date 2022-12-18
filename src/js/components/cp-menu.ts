682c5113636e74167eddbceafc80a4c3aac3565d
Manuel Strehl <git@manuel-strehl.de>
2022-12-18 17:04:21 +0100

    restructure main menu

--- src/js/components/cp-menu.ts
+++ src/js/components/cp-menu.ts
@@ -1,6 +1,10 @@
 import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import {unsafeSVG} from 'lit/directives/unsafe-svg.js';
+import IconGear from '@fortawesome/fontawesome-free/svgs/solid/gear.svg?raw';
+import IconHouse from '@fortawesome/fontawesome-free/svgs/solid/house.svg?raw';
+import IconScroll from '@fortawesome/fontawesome-free/svgs/solid/scroll.svg?raw';
+import IconShuffle from '@fortawesome/fontawesome-free/svgs/solid/shuffle.svg?raw';
 import IconXMark from '@fortawesome/fontawesome-free/svgs/solid/xmark.svg?raw';
 import { gettext as _ } from '../_i18n.ts';
 import { mixinBackdropClose } from '../_mixins.ts';
@@ -8,6 +12,14 @@ import { mixinBackdropClose } from '../_mixins.ts';
 @customElement('cp-menu')
 export class CpMenu extends LitElement {
   static styles = css`
+  a {
+    color: var(--color-link);
+    transition: color .3s;
+  }
+
+  a:focus, a:hover {
+    color: var(--color-link-hover);
+  }
   dialog {
     margin: 0;
     width: auto;
@@ -53,6 +65,27 @@ export class CpMenu extends LitElement {
     top: 1rem;
     right: 1rem;
   }
+  nav {
+    display: flex;
+    flex-wrap: wrap;
+    gap: .5rem;
+  }
+  nav > * {
+    min-width: 8rem;
+    text-align: center;
+  }
+  nav svg {
+    height: 3rem;
+    display: block;
+    fill: currentColor;
+    fill-opacity: .5;
+    margin: 0 auto .5rem;
+    transition: fill-opacity .3s;
+  }
+  nav >*:focus svg,
+  nav >*:hover svg {
+    fill-opacity: 1;
+  }
   `;
 
   render() {
@@ -63,13 +96,22 @@ export class CpMenu extends LitElement {
         ${unsafeSVG(IconXMark.replace('<svg ', '<svg width="42px" height="42px" ').replace('<path ', '<path fill="currentColor" '))}
         <span>${_('close')}</span>
       </button>
-      <a href="/">${_('go to the start page')}</a>
-      <a href="/random">${_('go to a random page')}</a>
+      <nav>
+        <a href="/">
+          ${unsafeSVG(IconHouse)}
+          ${_('start page')}</a>
+        <a href="/scripts">
+          ${unsafeSVG(IconScroll)}
+          ${_('scripts')}</a>
+        <a href="/random">
+          ${unsafeSVG(IconShuffle)}
+          ${_('random page')}</a>
+      </nav>
 
       <form method="get" action="/search">
         <p>
           <label>
-            ${_('Search codepoints.net:')}
+            ${_('Search code points:')}
             <input type="text" name="q" value="${query}">
           </label>
           <button type="submit">${_('search')}</button>
@@ -77,7 +119,9 @@ export class CpMenu extends LitElement {
       </form>
 
       <section>
-        <h2>${_('Settings')}</h2>
+        <h2>
+        ${unsafeSVG(IconGear.replace('<svg ', '<svg width="16px" height="16px" ').replace('<path ', '<path fill="currentColor" '))}
+        ${_('Settings')}</h2>
         <cp-darkmode></cp-darkmode>
         <cp-language></cp-language>
       </section>
