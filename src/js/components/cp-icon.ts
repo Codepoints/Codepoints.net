75f030d3d42f7179de76afb74f5e016ebd572b27
Manuel Strehl <git@manuel-strehl.de>
2022-12-28 22:08:10 +0100

    add comment

--- src/js/components/cp-icon.ts
+++ src/js/components/cp-icon.ts
@@ -2,6 +2,14 @@ import {LitElement, html, nothing} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import Icons from '../../images/icons.svg';
 
+/**
+ * render a specific icon from the sprite at src/images/icons.svg
+ *
+ * "icon" is the ID of the icon in the sprite
+ * "width" and "height" are the size of the icon. Any value needs to match
+ *     https://developer.mozilla.org/en-US/docs/Web/SVG/Content_type#length,
+ *     which excludes especially rem.
+ */
 @customElement('cp-icon')
 export class CpIcon extends LitElement {
   @property({ type: String })
