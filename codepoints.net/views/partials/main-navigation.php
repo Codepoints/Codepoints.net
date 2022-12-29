1bffe9608443abd488c57c3cbea5c60eeda0cdc7
Manuel Strehl <git@manuel-strehl.de>
2022-12-29 22:40:32 +0100

    fix gettext call

--- codepoints.net/views/partials/main-navigation.php
+++ codepoints.net/views/partials/main-navigation.php
@@ -14,11 +14,11 @@ if (! isset($nav) || ! $nav) {
 } ?>
 <?= array_key_exists('prev', $nav) ? $nav['prev'] : '<a href="/search">
   <cp-icon icon="magnifying-glass" width="16" height="16"></cp-icon>
-  <span class="title">'._('Search').'</span></a>' ?>
+  <span class="title">'.__('Search').'</span></a>' ?>
 <?= array_key_exists('up', $nav) ? $nav['up'] : '<a href="/planes">
   <svg width="16" height="16"><svg viewBox="194 97 1960 1960" width="100%" height="100%"><use href="'.static_url('images/unicode-logo-framed.svg#unicode').'" width="16" height="16"/></svg></svg>
-  <span class="title">'._('All Planes').'</span></a>' ?>
+  <span class="title">'.__('All Planes').'</span></a>' ?>
 <?= array_key_exists('next', $nav) ? $nav['next'] : '<a href="/random">
   <cp-icon icon="shuffle" width="16" height="16"></cp-icon>
-  <span class="title">'._('Random').'</span></a>' ?>
+  <span class="title">'.__('Random').'</span></a>' ?>
 </cp-navigation>
