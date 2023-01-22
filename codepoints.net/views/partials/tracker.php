27638e53202dba34a01265e88a705e68d985246f
Manuel Strehl <git@manuel-strehl.de>
2023-01-22 22:30:45 +0100

    add special tracking code to evaluate search terms

--- codepoints.net/views/partials/tracker.php
+++ codepoints.net/views/partials/tracker.php
@@ -1,6 +1,17 @@
+<?php
+/**
+ * @var string $view
+ * @var ?string $q
+ * @var ?\Codepoints\Unicode\SearchResult $search_result
+ */
+?>
 <script>var _paq=window._paq=window._paq||[];
 _paq.push(['disableCookies']);
+<?php if ($view === 'search' && isset($q) && $q && isset($search_result)): ?>
+_paq.push(['trackSiteSearch', <?=json_encode($q)?>, false, <?=$search_result->count()?>]);
+<?php else: ?>
 _paq.push(['trackPageView']);
+<?php endif ?>
 _paq.push(['enableLinkTracking']);
 (function(){
 var u="https://stats.codepoints.net/";
