b6ceb07e00a25473356bc3b3af485827069bcb0d
Manuel Strehl <m.strehl@kinetiqa.de>
2023-02-09 13:21:22 +0100

    try fixing the LCP hit by requesting the icons file very early

--- codepoints.net/views/partials/main-navigation.php
+++ codepoints.net/views/partials/main-navigation.php
@@ -5,7 +5,7 @@
 ?>
 <cp-navigation>
   <a href="<?=q(url(''))?>" rel="start" aria-label="<?=q('go to the homepage')?>">
-    <svg width="16" height="16"><use href="/static/images/icon.svg#icon"/></svg>
+    <svg width="64" height="64"><use href="/static/images/icon.svg#icon"/></svg>
     <span class="title"><?=_q('Home')?></span>
   </a>
 <?php
@@ -13,12 +13,12 @@ if (! isset($nav) || ! $nav) {
   $nav = [];
 } ?>
 <?= array_key_exists('prev', $nav) ? $nav['prev'] : '<a href="/search">
-  <cp-icon icon="magnifying-glass" width="16" height="16"></cp-icon>
+  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg#magnifying-glass').'"/></svg>
   <span class="title">'.__('Search').'</span></a>' ?>
 <?= array_key_exists('up', $nav) ? $nav['up'] : '<a href="/planes">
-  <svg width="16" height="16"><svg viewBox="194 97 1960 1960" width="100%" height="100%"><use href="'.static_url('images/unicode-logo-framed.svg#unicode').'" width="16" height="16"/></svg></svg>
+  <svg width="64" height="64" viewBox="194 97 1960 1960"><use href="'.static_url('images/unicode-logo-framed.svg#unicode').'" width="64" height="64"/></svg>
   <span class="title">'.__('All Planes').'</span></a>' ?>
 <?= array_key_exists('next', $nav) ? $nav['next'] : '<a href="/random">
-  <cp-icon icon="shuffle" width="16" height="16"></cp-icon>
+  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg#shuffle').'"/></svg>
   <span class="title">'.__('Random').'</span></a>' ?>
 </cp-navigation>
