d342d217c78a01a0bd0227e49a3e07f88a5f1309
Manuel Strehl <git@manuel-strehl.de>
2023-02-09 16:19:19 +0100

    fix URLs from static_url()

--- codepoints.net/views/partials/main-navigation.php
+++ codepoints.net/views/partials/main-navigation.php
@@ -13,12 +13,12 @@ if (! isset($nav) || ! $nav) {
   $nav = [];
 } ?>
 <?= array_key_exists('prev', $nav) ? $nav['prev'] : '<a href="/search">
-  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg#magnifying-glass').'"/></svg>
+  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg').'#magnifying-glass"/></svg>
   <span class="title">'.__('Search').'</span></a>' ?>
 <?= array_key_exists('up', $nav) ? $nav['up'] : '<a href="/planes">
   <svg width="64" height="64" viewBox="194 97 1960 1960"><use href="'.static_url('images/unicode-logo-framed.svg#unicode').'" width="64" height="64"/></svg>
   <span class="title">'.__('All Planes').'</span></a>' ?>
 <?= array_key_exists('next', $nav) ? $nav['next'] : '<a href="/random">
-  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg#shuffle').'"/></svg>
+  <svg width="64" height="64"><use href="'.static_url('src/images/icons.svg').'#shuffle"/></svg>
   <span class="title">'.__('Random').'</span></a>' ?>
 </cp-navigation>
