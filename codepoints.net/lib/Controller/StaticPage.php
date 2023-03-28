e7e5c6a0f1a76d69b888cb1daf8aa83dc97f2787
Manuel Strehl <git@manuel-strehl.de>
2023-03-28 21:46:21 +0200

    simplify the static pages controller

--- codepoints.net/lib/Controller/StaticPage.php
+++ codepoints.net/lib/Controller/StaticPage.php
@@ -8,38 +8,35 @@ use Codepoints\View;
 
 
 class StaticPage extends Controller {
-
-    private Array $views;
-
-    public function __construct() {
-        $this->views = [
-            'about' => [
-                'title' => __('About Codepoints'),
-                'page_description' => __('Codepoints is a site dedicated to Unicode. This page explains the concepts and possibilities to navigate Unicode on the site.'),
-            ],
-            'glossary' => [
-                'title' => __('Glossary of Terms'),
-                'page_description' => __('This glossary explains central terms of the Unicode standard and character encodings in general.'),
-            ],
-            'offline.html' => [
-                'title' => __('You are off-line'),
-                'page_description' => __('You are currently off-line. Pages, that you visited recently, should still be available, though.'),
-            ],
-        ];
-    }
-
     /**
      * @param string $view
      */
     public function __invoke($view, Array $env) : string {
-        if (! array_key_exists($view, $this->views)) {
+        switch ($view) {
+        case 'about':
+            $data = $env['db']->getOne('SELECT COUNT(*) AS c FROM codepoints USE INDEX (PRIMARY)');
+            $context = [
+                'title' => __('About Codepoints'),
+                'page_description' => __('Codepoints is a site dedicated to Unicode. This page explains the concepts and possibilities to navigate Unicode on the site.'),
+                'cp_count' => $data['c'],
+            ];
+            break;
+        case 'glossary':
+            $context = [
+                'title' => __('Glossary of Terms'),
+                'page_description' => __('This glossary explains central terms of the Unicode standard and character encodings in general.'),
+            ];
+            break;
+        case 'offline.html':
+            $context = [
+                'title' => __('You are off-line'),
+                'page_description' => __('You are currently off-line. Pages, that you visited recently, should still be available, though.'),
+            ];
+            break;
+        default:
             throw new NotFoundException('This page is unknown');
         }
-        if ($view === 'about') {
-            $data = $env['db']->getOne('SELECT COUNT(*) AS c FROM codepoints USE INDEX (PRIMARY)');
-            $this->views['about']['cp_count'] = $data['c'];
-        }
-        return (new View(basename($view, '.html')))($this->views[$view], $env);
+        return (new View(basename($view, '.html')))($context, $env);
     }
 
 }
