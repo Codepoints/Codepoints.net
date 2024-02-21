c9b9a635d7cfb9183eb2577f2dd752047cc5c753
Manuel Strehl <git@manuel-strehl.de>
2024-02-21 22:59:00 +0100

    instead of caching produce actual files for the web server to serve

--- codepoints.net/lib/Controller/Image.php
+++ codepoints.net/lib/Controller/Image.php
@@ -2,10 +2,6 @@
 
 namespace Codepoints\Controller;
 
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Component\Cache\Marshaller\DefaultMarshaller;
-use Symfony\Component\Cache\Marshaller\DeflateMarshaller;
-use Symfony\Contracts\Cache\ItemInterface;
 use Codepoints\Controller;
 
 class Image extends Controller {
@@ -14,26 +10,25 @@ class Image extends Controller {
      * @param Array $match
      */
     public function __invoke($match, Array $env) : string {
-        /* note, that users may see twice this value in the worst case. That
-         * is, a cached entry shortly before going stale but an Expires header,
-         * that starts fresh. */
         $cache_duration = DEBUG? 10 : 24*60*60;
         $root = hexdec($match[1]);
+        $canonical = str_pad(strtoupper(dechex($root)), 4, '0', STR_PAD_LEFT);
         header('Content-Type: image/svg+xml');
         header('Cache-Control: public, max-age='.$cache_duration);
         header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $cache_duration));
-        $marshaller = new DeflateMarshaller(new DefaultMarshaller());
-        $cache = new FilesystemAdapter('codepts', 0, null, $marshaller);
-        return $cache->get('controller_image_'.dechex($root), function (ItemInterface $item) use($env, $root, $cache_duration) {
-            $item->expiresAfter($cache_duration);
-            $images = $env['db']->getAll('SELECT width, height, image
-                FROM codepoint_image
-                WHERE cp >= ? AND cp < ? + 256', $root, $root);
-            return sprintf(
-            '<svg xmlns="http://www.w3.org/2000/svg">'.
-                '<style>:root svg{opacity:0;height:100%%;width:auto}:root svg:target{opacity:1}</style>'.
-                '%s'.
-                '</svg>', join('', array_map(function(Array $image) : string { return $image['image']; }, $images)));
-        });
+        $images = $env['db']->getAll('SELECT width, height, image
+            FROM codepoint_image
+            WHERE cp >= ? AND cp < ? + 256', $root, $root);
+        $svg = sprintf(
+        '<svg xmlns="http://www.w3.org/2000/svg">'.
+            '<style>:root svg{opacity:0;height:100%%;width:auto}:root svg:target{opacity:1}</style>'.
+            '%s'.
+            '</svg>', join('', array_map(function(Array $image) : string { return $image['image']; }, $images)));
+        /* let the web server take over by storing already created images in
+         * their place in the docroot */
+        @file_put_contents(
+            dirname(dirname(__DIR__)).'/image/'.$canonical.'.svg',
+            $svg);
+        return $svg;
     }
 }
