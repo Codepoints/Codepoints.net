568eb8046aaac4591b8017f2931901e54cf75f9c
Manuel Strehl <git@manuel-strehl.de>
2024-02-25 22:26:07 +0100

    s/Twitter/Mastodon/ on 500 error page

--- codepoints.net/views/500.php
+++ codepoints.net/views/500.php
@@ -2,8 +2,8 @@
 <main class="main main--500">
   <h1><?=q($title)?></h1>
   <p><?=q($page_description ?? '')?></p>
-  <p><?=sprintf(_q('If this problem persists, please tell us on %sTwitter%s or via %se-mail%s.'),
-      '<a href="https://twitter.com/CodepointsNet">',
+  <p><?=sprintf(_q('If this problem persists, please tell us on %sMastodon%s or via %se-mail%s.'),
+      '<a href="https://typo.social/@codepoints">',
       '</a>',
       '<a href="https://manuel-strehl.de/contact">',
       '</a>',
