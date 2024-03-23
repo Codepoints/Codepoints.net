1eddec0b116694be643aa4dc5c80b040cedc4586
Manuel Strehl <git@manuel-strehl.de>
2024-03-23 19:35:47 +0100

    add cli command to create description
    For testing purposes.

--- codepoints.net/lib/CommandLine.php
+++ codepoints.net/lib/CommandLine.php
@@ -3,6 +3,21 @@
 namespace Codepoints;
 
 use Codepoints\Search\Documenter;
+use Codepoints\Unicode\Codepoint;
+use Codepoints\Unicode\CodepointInfo\Aliases;
+use Codepoints\Unicode\CodepointInfo\CLDR;
+use Codepoints\Unicode\CodepointInfo\CSUR;
+use Codepoints\Unicode\CodepointInfo\Confusables;
+use Codepoints\Unicode\CodepointInfo\Description;
+use Codepoints\Unicode\CodepointInfo\Extra;
+use Codepoints\Unicode\CodepointInfo\ImageSource;
+use Codepoints\Unicode\CodepointInfo\OtherSites;
+use Codepoints\Unicode\CodepointInfo\Pronunciation;
+use Codepoints\Unicode\CodepointInfo\Properties;
+use Codepoints\Unicode\CodepointInfo\Relatives;
+use Codepoints\Unicode\CodepointInfo\Representation;
+use Codepoints\Unicode\CodepointInfo\Sensitivity;
+use Codepoints\Unicode\CodepointInfo\Wikipedia;
 
 
 class CommandLine {
@@ -13,7 +28,7 @@ class CommandLine {
     public function run() : int {
         $method = str_replace('-', '_', $this->argv[1] ?? 'help');
         if (method_exists($this, $method)) {
-            return call_user_func([$this, $method]);
+            return call_user_func([$this, $method], ...array_slice($this->argv, 2));
         }
         echo "no matching method found\n";
         $this->help();
@@ -30,6 +45,27 @@ EOF;
 
     private function build_search() : int {
         (new Documenter($this->env))->buildNext();
+        return 0;
+    }
+
+    private function describe(string $cp) : int {
+        new Aliases($this->env);
+        new CLDR($this->env);
+        new CSUR($this->env);
+        new Confusables($this->env);
+        new Description($this->env);
+        new Extra($this->env);
+        new ImageSource($this->env);
+        new OtherSites($this->env);
+        new Pronunciation($this->env);
+        new Properties($this->env);
+        new Relatives($this->env);
+        new Representation($this->env);
+        new Sensitivity();
+        new Wikipedia($this->env);
+        $cp = preg_replace('/^u\+/i', '', $cp);
+        $cpo = get_codepoint(hexdec($cp), $this->env['db']);
+        echo preg_replace('/\s*\n\n\s+/', "\n\n", preg_replace('/[ \t]+/', ' ', strip_tags($cpo->description ?? "- no description -\n")));
         return 0;
     }
 
