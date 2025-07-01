8a82aa0e253bc938b0a9fe22e8a36abd4d7ca08f
Manuel Strehl <git@manuel-strehl.de>
2025-07-01 23:08:51 +0200

    fix psalm issues

--- codepoints.net/lib/Router/RateLimiter.php
+++ codepoints.net/lib/Router/RateLimiter.php
@@ -19,16 +19,20 @@ class RateLimiter {
             private Database $db
     ) {
         $this->ip = filter_input(INPUT_SERVER, 'HTTP_X_FORWARDED_FOR') ?? null;
-        if (! $this->ip) {
-            return;
-        }
         $this->doLimit();
     }
 
-    private function doLimit() {
+    private function doLimit() : void {
+        /** @psalm-suppress RiskyTruthyFalsyComparison */
+        if (! $this->ip) {
+            /* without a client IP we cannot proceed here */
+            return;
+        }
+
         $data = $this->db->getOne('SELECT hits
             FROM rate_limit
             WHERE ip = ? AND timerange = ?', $this->ip, $this->timerange);
+        /** @psalm-suppress RiskyTruthyFalsyComparison */
         if ($data && $data['hits'] > $this->limit) {
             $this->dbExec(
                 'UPDATE rate_limit
@@ -47,8 +51,11 @@ class RateLimiter {
             ', $this->ip, $this->timerange);
     }
 
-    private function dbExec($query, ...$params) {
+    private function dbExec(string $query, string|int ...$params) : bool {
         $statement = $this->db->prepare($query);
+        if (! $statement) {
+            return false;
+        }
         return $statement->execute($params);
     }
 
@@ -57,7 +64,7 @@ class RateLimiter {
      *
      * Meant to be run in a cronjob.
      */
-    public static function clearStale(Database $db) {
+    public static function clearStale(Database $db) : void {
         $db->query('DELETE FROM rate_limit WHERE last_seen < DATE_SUB(now(), INTERVAL timerange SECOND)');
     }
 
--- tests/psalm_stubs.php
+++ tests/psalm_stubs.php
@@ -2,3 +2,5 @@
 
 define( 'SOFTWARE_VERSION', 'string' );
 define( 'UNICODE_VERSION', 'string' );
+
+class GdImage {}
