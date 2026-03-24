2d4fd9b1abf408780775ca523b35b19c51bcf177
Manuel Strehl <git@manuel-strehl.de>
2026-03-24 21:27:26 +0100

    refine social images

--- codepoints.net/lib/Controller/OGImage.php
+++ codepoints.net/lib/Controller/OGImage.php
@@ -6,6 +6,8 @@ use Imagick;
 use ImagickDraw;
 use ImagickPixel;
 use Codepoints\Controller;
+use Codepoints\Unicode\Codepoint;
+use Codepoints\Unicode\CodepointInfo\SENSITIVITY_LEVEL;
 
 final class OGImage extends Controller {
 
@@ -14,29 +16,29 @@ final class OGImage extends Controller {
      */
     #[\Override]
     public function __invoke($match, Array $env) : string {
+        $cp = Codepoint::getCached($env['db']->getOne('SELECT cp, gc, name
+            FROM codepoints
+            WHERE cp = ?', hexdec($match[1])), $env['db']);
+
         /* send an expiry of 1 year. This mirrors the Apache config for the
          * cached files. */
         $cache_duration = 365*24*60*60;
-        $root = hexdec($match[1]);
-        $info = $env['db']->getOne('SELECT gc, name
-            FROM codepoints
-            WHERE cp = ?', $root);
-        $canonical = str_pad(strtoupper(dechex($root)), 4, '0', STR_PAD_LEFT);
         header('Content-Type: image/png');
         header('Cache-Control: public, max-age='.$cache_duration);
         header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $cache_duration));
+
         $dbimage = $env['db']->getOne('SELECT width, height, image
             FROM codepoint_image
-            WHERE cp = ?', get_printable_codepoint($root, $info['gc']));
+            WHERE cp = ?', get_printable_codepoint($cp->id, $cp->gc));
         if (! $dbimage) {
             http_response_code(404);
             return '';
         }
-        $svg = sprintf(
-        '<svg xmlns="http://www.w3.org/2000/svg" width="%s" height="%s" viewBox="0 0 1000 1000">'.
-            '<style>:root svg{opacity:0;height:100%%;width:auto}:root svg:target{opacity:1}</style>'.
-            '%s'.
-            '</svg>', $dbimage['width'], $dbimage['height'], $dbimage['image']);
+        $svg = preg_replace(
+            '/viewBox="[^"]+"/',
+            sprintf('viewBox="-50 0 %s %s"', $dbimage['width'] + 100, $dbimage['height'] + 100),
+            $dbimage['image']);
+        $ratio = ($dbimage['width'] + 100) / ($dbimage['height'] + 100);
 
         $img = new Imagick();
         $img->setSize(1200, 630);
@@ -46,26 +48,36 @@ final class OGImage extends Controller {
         $svgimg->setBackgroundColor('transparent');
         $svgimg->readImageBlob($svg);
         $svgimg->setImageFormat('png24');
-        $svgimg->resizeImage(250, 250, imagick::FILTER_LANCZOS, 1);
-        $img->compositeImage($svgimg, Imagick::COMPOSITE_DEFAULT, 100 + (250 - $svgimg->getImageWidth()) / 2, 100);
+        if ($cp->sensitivity->value !== SENSITIVITY_LEVEL::NORMAL->value) {
+            $blur = match($cp->sensitivity->value) {
+                SENSITIVITY_LEVEL::RAISED->value => 50,
+                SENSITIVITY_LEVEL::HIGH->value => 80,
+                SENSITIVITY_LEVEL::MAX->value => 100,
+                default => 0,
+            };
+            $svgimg->blurImage($blur, $blur);
+        }
+        $svgimg->resizeImage(ceil(min(400, 400 * $ratio)), ceil(min(400, 400 / $ratio)), imagick::FILTER_LANCZOS, 1);
+        $img->compositeImage($svgimg, Imagick::COMPOSITE_DEFAULT, 50 + (int)((400 - $svgimg->getImageWidth()) / 2), 50);
 
-        $name = case_cp_name($info['name']);
+        $name = case_cp_name($cp->name);
         $draw = new ImagickDraw();
         $draw->setFillColor('#1c1917');
         $draw->setFont(dirname(dirname(__DIR__)).static_url('src/fonts/Literata.woff2'));
 
         $draw->setFontSize(100);
-        $img->annotateImage($draw, 400, 350, 0, sprintf('U+%04X', $root));
+        $img->annotateImage($draw, 500, 300, 0, sprintf('U+%04X', $cp->id));
 
         $draw->setFontSize(strlen($name) < 34? 60 : 40);
-        $img->annotateImage($draw, 75, 500, 0, $name);
+        $img->annotateImage($draw, 50, 530, 0, $name);
 
         $draw->setFillColor('rgb(95, 35, 40)');
         $draw->setFontSize(30);
-        $img->annotateImage($draw, 75, 570, 0, sprintf('Read more on codepoints.net/U+%04X', $root));
+        $img->annotateImage($draw, 50, 590, 0, sprintf('Read more on codepoints.net/U+%04X', $cp->id));
 
         /* let the web server take over by storing already created images in
          * their place in the docroot */
+        $canonical = str_pad(strtoupper(dechex($cp->id)), 4, '0', STR_PAD_LEFT);
         $img->writeImage(dirname(dirname(__DIR__)).'/image/og-'.$canonical.'.png');
         return $img->getImageBlob();
     }
