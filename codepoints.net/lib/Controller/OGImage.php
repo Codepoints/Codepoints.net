1ec3937ab4919ceccceef4c4422bd9bfe8bd1bf0
Manuel Strehl <git@manuel-strehl.de>
2026-03-28 17:51:16 +0100

    catch error cases when generating OG images

--- codepoints.net/lib/Controller/OGImage.php
+++ codepoints.net/lib/Controller/OGImage.php
@@ -16,24 +16,26 @@ final class OGImage extends Controller {
      */
     #[\Override]
     public function __invoke($match, Array $env) : string {
-        $cp = Codepoint::getCached($env['db']->getOne('SELECT cp, gc, name
-            FROM codepoints
-            WHERE cp = ?', hexdec($match[1])), $env['db']);
-
-        /* send an expiry of 1 year. This mirrors the Apache config for the
-         * cached files. */
-        $cache_duration = 365*24*60*60;
         header('Content-Type: image/png');
-        header('Cache-Control: public, max-age='.$cache_duration);
-        header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $cache_duration));
+        /* in the error case we send a 1x1 black pixel PNG w/o caching headers */
 
-        /** @var ?Array{width: int, height: int, image: string} */
+        /** @var Array{cp: int, gc: string, name: string}|false */
+        $cp_data = $env['db']->getOne('SELECT cp, gc, name
+            FROM codepoints
+            WHERE cp = ?', hexdec($match[1]));
+        if (! $cp_data) {
+            http_response_code(404);
+            return base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAACklEQVR4AWNgAAAAAgABc3UBGAAAAABJRU5ErkJggg==');
+        }
+        $cp = Codepoint::getCached($cp_data, $env['db']);
+
+        /** @var Array{width: int, height: int, image: string}|false */
         $dbimage = $env['db']->getOne('SELECT width, height, image
             FROM codepoint_image
             WHERE cp = ?', get_printable_codepoint($cp->id, $cp->gc));
         if (! $dbimage) {
             http_response_code(404);
-            return '';
+            return base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAACklEQVR4AWNgAAAAAgABc3UBGAAAAABJRU5ErkJggg==');
         }
         $svg = (string)preg_replace(
             '/viewBox="[^"]+"/',
@@ -81,6 +83,12 @@ final class OGImage extends Controller {
          * their place in the docroot */
         $canonical = str_pad(strtoupper(dechex($cp->id)), 4, '0', STR_PAD_LEFT);
         $img->writeImage(dirname(dirname(__DIR__)).'/image/og-'.$canonical.'.png');
+
+        /* send an expiry of 1 year. This mirrors the Apache config for the
+         * cached files. */
+        $cache_duration = 365*24*60*60;
+        header('Cache-Control: public, max-age='.$cache_duration);
+        header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + $cache_duration));
         return $img->getImageBlob();
     }
 }
